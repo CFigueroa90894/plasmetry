74c99f3261e2097b1bf5a0788409b579fd141376
Alberto <albertocases@e-3consulting.com>
2024-08-07 14:08:45 -0400

    testing sweep lists

---
+++ src/diagnostics/calculations/param_test/calculate_parameters.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  7 13:43:38 2024
+
+@author: ajco2
+"""
+
+
+import slp_plasma_parameters as slp
+from scipy import signal
+
+    
+def FilterSignal(rawSignal):
+    """
+    Filters a signal using a butterworth digital filter. 
+    
+    Inputs:
+        rawSignal = an array of raw data captured by the sensor.
+        
+    Outputs:
+        filteredSignal = signal after being processed by a butterworth digital filter.
+    """
+    
+    sos = signal.butter(2, 0.03, output='sos')
+    filteredSignal = signal.sosfiltfilt(sos, rawSignal)
+    
+    return filteredSignal
+
+def get_sweeps_parameters(sweep_list):
+    parameter_list = []
+    for sweep in sweep_list: 
+        current = sweep[0]
+        bias = sweep[1]
+        parameter_dictionary = {} 
+        #filtering raw current, storing the output in a list
+        filtered_current = FilterSignal(current)
+
+
+        floating_potential, plasma_potential = slp.get_floating_and_plasma_potential(filtered_current, bias)
+
+        parameter_dictionary['floating potential (V)']= floating_potential[1]
+
+        parameter_dictionary['plasma potential (V)']= plasma_potential[1]
+
+        electron_temperature_ev, electron_temperature_joules = slp.get_electron_temperature(floating_potential, plasma_potential, filtered_current, bias)
+
+        parameter_dictionary['electron temperature (eV)']=  electron_temperature_ev
+        parameter_dictionary['electron temperature (J)'] = electron_temperature_joules
+        
+
+        electron_saturation_current = slp.get_electron_saturation_current(filtered_current, plasma_potential[0])
+
+        probe_area = 30.3858e-06
+
+        electron_density= slp.get_electron_density(electron_saturation_current, electron_temperature_joules, probe_area)
+
+        parameter_dictionary['Electron Density']= electron_density
+
+        debye_length = slp.get_debye_length(electron_temperature_joules, electron_density)
+
+
+        parameter_dictionary['Debye length (m)']= debye_length
+        
+        number_of_charged_particles = slp.get_number_of_electrons(debye_length, electron_density)
+
+        parameter_dictionary['Number of charged particles in the Debye sphere (integer)']= number_of_charged_particles
+        parameter_list.append(parameter_dictionary)
+        
+    return parameter_list
---
+++ src/diagnostics/calculations/testing/params_to_csv.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  7 13:23:17 2024
+
+@author: ajco2
+"""
+import calculate_parameters as slp
+import csv
+#import sweep_parser as parser
+# Save a separate CSV for plasma parameters
+def ParametersToCsv(listOfParameters, fname):
+   
+   keys = list(listOfParameters[0].keys())
+   
+
+   with open( fname, "w", newline='') as csv_file:
+       dict_writer= csv.DictWriter(csv_file, keys)
+       dict_writer.writeheader()
+       dict_writer.writerows(listOfParameters)
+
+def LoadPreviousData():
+    with open('Feliz_A1 MirorSLP120200813T105858.csv', newline='') as csvfile:
+        dataReader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        next(dataReader)  # Skip the header row
+        current = []
+        voltageSLP = []
+        for row in dataReader:
+            try:
+                current.append(float(row[0]))
+                voltageSLP.append(float(row[1]))
+            except:
+                None    
+        
+        return voltageSLP, current
+
+def main():
+    sweep_list = []
+    list_of_sweeps = []
+    bias, current = LoadPreviousData()
+    sweep_list.append(current)
+    sweep_list.append(bias)
+    list_of_sweeps.append(sweep_list)
+    
+    print(sweep_list)
+    parameter_list = slp.get_sweeps_parameters(list_of_sweeps)
+    filename = 'slp parameters from test.csv'
+    ParametersToCsv(parameter_list, filename)
+    print(parameter_list)
+    
+    
+main()
+
+
+
+
