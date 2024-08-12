50440ba3c8366e766e96add7efecf38af93c2709
Alberto <albertocases@e-3consulting.com>
2024-08-12 13:33:14 -0400

    factory update

--- src/diagnostics/probe_objects/probe_factory.py
+++ src/diagnostics/probe_objects/probe_factory.py
@@ -1,11 +1,29 @@
-from Single_Langmuir_Probe import Single_Langmuir_Probe
 
-def get_probe_object(probe_number):
-    
-    probe_objects = {
-    
-        1: Single_Langmuir_Probe(),
+from slp_plasma_parameters import get_equations as slp_equations
+from dlp_plasma_parameters import get_equations as dlp_equations
+from tlv_plasma_parameters import get_equations as tlv_equations
+from global_parameters import get_equations as analyzers_equations
+
+from LangmuirProbe import LangmuirProbe
+from BaseTLP import BaseTLP
+from EnergyAnalyzers import EnergyAnalyzers
+from ProbeEnum import PRB        
+
+class ProbeFactory:
+    def __init__(self, probe_type: PRB):
+        match probe_type:
+            case PRB.SLP:
+                self.probe= LangmuirProbe(slp_equations)
+            case PRB.DLP:
+                self.probe= LangmuirProbe(dlp_equations)
+            case PRB.HEA: 
+                self.probe= EnergyAnalyzers(analyzers_equations)
+            case PRB.IEA: 
+                self.probe= EnergyAnalyzers(analyzers_equations)
+            case PRB.TLV:
+                self.probe= BaseTLP(tlv_equations)
+            case _:
+                raise ValueError(f"Unknown probe type: {self.probe_type}")
         
-    }
-    return probe_objects[probe_number]  
-    
+
+
