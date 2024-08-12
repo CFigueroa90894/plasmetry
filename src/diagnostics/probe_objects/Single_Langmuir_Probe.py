cccf8f016f9b5ca5dcc32d886732e01fd9559973
Alberto <albertocases@e-3consulting.com>
2024-08-12 08:30:10 -0400

    Update Single_Langmuir_Probe.py

--- src/diagnostics/probe_objects/Single_Langmuir_Probe.py
+++ src/diagnostics/probe_objects/Single_Langmuir_Probe.py
@@ -2,14 +2,16 @@ from threading import Event     # built-in
 import queue
 from slp_plasma_parameters import get_equations
 import Langmuir_Probe
-# TO DO - ENFORCE ABSTRACT, and others
 class Single_Langmuir_Probe(Langmuir_Probe):
-    # TO DO
+    
     def __init__(self):
+        
         super().__init__()
+        
         self.equations = get_equations()
-        # TO DO
+        
         self.data_buffer= queue.Queue()
+        
         self.clock_flag = Event()
         #self.probe_type = PRB.SLP
         
