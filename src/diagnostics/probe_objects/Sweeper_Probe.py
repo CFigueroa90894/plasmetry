1e26d2522529b08d9a9cf2f0e6c6d6f46cb453cf
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-08-20 18:11:19 -0400

    Created skeleton for SweeperProbe class in Sweeper_Probe module.

---
+++ src/diagnostics/probe_objects/Sweeper_Probe.py
@@ -0,0 +1,33 @@
+# author: figueroa_90894@studnt.pupr.edu
+# status: WIP
+#   - add subcomponents once hardware interface is implemented
+#   - implement sweep method
+
+# local imports
+from Base_Probe import BaseProbe        # parent class
+
+
+class SweeperProbe(BaseProbe):
+    """<...>"""
+    def __init__(self,
+                 num_samples:int,
+                 *args, **kwargs
+                 ):
+        super().__init__(*args, **kwargs)   # initialize attributes inherited from parent
+        
+        # PROBE INFO
+        self.num_samples = num_samples      # number of samples to obtain per sweep
+
+        # TO DO - PROBE SUBCOMPONENTS
+        self._sweeper = None        # Hardware interface obj - TBD
+        self._collector = None      # Hardware interface obj - TBD
+
+    # TO DO
+    def sweep(self) -> dict:
+        """Performs a single voltage sweep on the sweeper object.
+        Returns a dictionary consisting of applied biases and raw sampled voltages."""
+        raise NotImplementedError
+
+
+
+
