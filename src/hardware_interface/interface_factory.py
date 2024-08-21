2fa272cc98edd86df34df207c072a04cd18f8d0f
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-08-21 12:48:35 -0400

    Created InterfaceFactory class to instantiate I/O objects and decouple them from specific hardware interface implementations.

---
+++ src/hardware_interface/interface_factory.py
@@ -0,0 +1,43 @@
+# author: figueroa_90894@students.pupt.edu
+# status: WIP
+#   - add docstrings
+
+# built-in imports
+from enum import Enum, unique
+
+# local imports
+from abstract_interface import AbstractInterface as interface
+from interface_objects import AnalogIn, AnalogOut, DigitalIn, DigitalOut
+
+# ----- CHANNEL TYPES ----- #
+@unique
+class CHN(Enum):
+    """<...>"""
+    DO = 0
+    AO = 1
+    DI = 2
+    AI = 3
+
+# ----- FACTORY ----- #
+class InterfaceFactory:
+    def __init__(self, hardware_cls:interface):
+        """<...>
+        <hardware must be class, not obj>"""
+        self.hardware = hardware_cls()
+
+    def Channel(self, address:int, type:CHN):
+        """<...>"""
+        match type:
+            # Outputs
+            case CHN.DO:
+                return DigitalOut(address=address, hardware_obj=self.hardware)
+            case CHN.AO:
+                return AnalogOut(address=address, hardware_obj=self.hardware)
+            
+            # Inputs
+            case CHN.DI:
+                return DigitalIn(address=address, hardware_obj=self.hardware)
+            case CHN.AI:
+                return AnalogIn(address=address, hardware_obj=self.hardware)
+                  
+                  
