62c2b1e665a122272875c1910a3ee8423fc5ed20
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-08-21 01:34:22 -0400

    Added skeleton for EnergyAnalzyer class in Energy_Analyzer module.

---
+++ src/diagnostics/probe_objects/Energy_Analyzer.py
@@ -0,0 +1,27 @@
+# author: figueroa_90894@studnt.pupr.edu
+# status: WIP
+
+
+# local imports
+from Sweeper_Probe import SweeperProbe
+from ProbeEnum import PRB
+
+
+class EnergyAnalyzer(SweeperProbe):
+    """<...>"""
+    def __init__(self,
+                 probe_type:PRB,
+                 *args, **kwargs):
+        """<...>"""
+        super().__init__(*args, **kwargs)
+
+        # PROBE INFO
+        self.probe_type = probe_type
+
+        # TO DO
+        self.collimator = None  #hardware interface
+
+    def run(self):
+        """<...>"""
+        raise NotImplementedError
+
