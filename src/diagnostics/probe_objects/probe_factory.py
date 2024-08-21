8ee72388320b24f278d966018dda7511e11814ac
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-08-21 10:52:30 -0400

    Modified ProbeFactory to use the implemented concrete classes.

--- src/diagnostics/probe_objects/probe_factory.py
+++ src/diagnostics/probe_objects/probe_factory.py
@@ -1,30 +1,42 @@
+# author:   cases_123268@students.pupr.edu
+# modified: figueroa_90894@students.pupr.edu
+# status: WIP
+#   - add docstrings
+#   - resolve equation imports
 
+# ----- LOCAL IMPORTS ----- #
+# PARAMETER EQUATIONS
 from slp_plasma_parameters import get_equations as slp_equations
 from dlp_plasma_parameters import get_equations as dlp_equations
 from tlv_plasma_parameters import get_equations as tlv_equations
 from tlc_plasma_parameters import get_equations as tlc_equations
 from global_parameters import get_equations as analyzers_equations
 
-from LangmuirProbe import LangmuirProbe
-from BaseTLP import BaseTLP
-from EnergyAnalyzers import EnergyAnalyzers
+# CONCRETE PROBE OBJECTS
+from Langmuir_Probe import LangmuirProbe
+from Triple_Lang_Voltage import TripleLangVoltage
+from Triple_Lang_Current import TripleLangCurrent
+from Energy_Analyzer import EnergyAnalyzer
 
+# PROBE IDENTIFIERS
 from ProbeEnum import PRB        
 
+
 class ProbeFactory:
+    "<...>"
     def __new__(self, probe_type: PRB):
         match probe_type:
             case PRB.SLP:
-                probe= LangmuirProbe(slp_equations)
+                probe = LangmuirProbe(equations=slp_equations)
             case PRB.DLP:
-                probe= LangmuirProbe(dlp_equations)
-            case PRB.HEA | PRB.IEA: 
-                probe= EnergyAnalyzers(analyzers_equations)
+                probe = LangmuirProbe(equations=dlp_equations)
+            case PRB.HEA | PRB.IEA:     # how to select particle type for HEA?
+                probe = EnergyAnalyzer(equations=analyzers_equations)
             case PRB.TLV:
-                probe= BaseTLP(tlv_equations)
+                probe = TripleLangVoltage(equations=tlv_equations)
             case PRB.TLC:
-                probe= BaseTLP(tlc_equations)
-            case _:
+                probe = TripleLangCurrent(equations=tlc_equations)
+            case _: # edge case handling
                 raise ValueError(f"Unknown probe type: {self.probe_type}")
         return probe
 
