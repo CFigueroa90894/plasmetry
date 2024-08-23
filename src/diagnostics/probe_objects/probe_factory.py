79d3f7e65bd0dc0f17d3bffe59cd1d4a2263fbd6
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-08-23 17:42:34 -0400

    Moved Probe ID enumerator class to probe_factory.py, deleted separate ProbeEnum file.

--- src/diagnostics/probe_objects/probe_factory.py
+++ src/diagnostics/probe_objects/probe_factory.py
@@ -4,6 +4,8 @@
 #   - add docstrings
 #   - resolve equation imports
 
+from enum import Enum, unique
+
 # ----- LOCAL IMPORTS ----- #
 # PARAMETER EQUATIONS
 from slp_plasma_parameters import get_equations as slp_equations
@@ -18,8 +20,18 @@ from concrete_probes.Triple_Lang_Voltage import TripleLangVoltage
 from concrete_probes.Triple_Lang_Current import TripleLangCurrent
 from concrete_probes.Energy_Analyzer import EnergyAnalyzer
 
+
 # PROBE IDENTIFIERS
-from ProbeEnum import PRB        
+@unique # prevent duplicate values
+class PRB(Enum):
+    """An enumarator class to represent probe types."""
+    ABS = 0     # abstract probe type
+    SLP = 1     # Single Langmuir Probe
+    DLP = 2     # Double Langmuir Probe
+    HEA = 3     # Hyperbolic Energy Analyzer
+    IEA = 4     # Ion Energy Analyzer
+    TLC = 5     # Triple Langmuir Probe - Current Mode
+    TLV = 6     # Triple Langmuir Probe - Voltage Mode    
 
 
 class ProbeFactory:
