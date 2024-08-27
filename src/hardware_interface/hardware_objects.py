1ad7d1a42ef05efb3effd30ab198539e400038bc
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-08-27 16:06:15 -0400

    Updated multiple files with path hammer v2.4, and modified their impoorts accordingly: calculations_factory.py, Base_TLP.py, Sweeper_Probe.py, Energy_Analyzer.py, Langmuir_Probe.py, Triple_Lang_Current.py, Triple_Lang_Voltage.py, probe_factory.py, hardware_factory.py, and hardware_objects.py.

--- src/hardware_interface/hardware_objects.py
+++ src/hardware_interface/hardware_objects.py
@@ -6,22 +6,22 @@
 import sys
 import os
 
-# ----- PATH HAMMER ----- resolve absolute imports ----- #
+# ----- PATH HAMMER v2.4 ----- resolve absolute imports ----- #
 if __name__ == "__main__":  # execute snippet if current script was run directly 
-    num_dir = 2             # how many parent folders to reach /plasmetry/src
-    
-    # Locate absolute path to /plasmetry/src
-    parent = os.path.dirname(__file__)  # traverse directory upwards
-    for _ in range(num_dir): parent = os.path.dirname(parent)
-    print(f"Path Hammer: {parent}")     # expect absolute path to /plasmetry/src
+    num_dir = 1             # how many parent folders to reach /plasmetry/src
 
-    # Append all target folders to python path
-    for dir in os.listdir(parent): sys.path.append(f"{parent}/{dir}")
-    print(f"Path Hammer: folders appended")
+    src_abs = os.path.abspath(os.path.dirname(__file__) + num_dir*'/..') # absolute path to plasmetry/src
+    print(f"Path Hammer: {src_abs}")
+    split = src_abs.split('\\')     # separate path into folders for validation
+    assert split[-2] == 'plasmetry' and split[-1] == 'src'  # validate correct top folder
+    
+    targets = [x[0] for x in os.walk(src_abs) if x[0].split('\\')[-1]!='__pycache__'] # get subdirs, exclude __pycache__
+    for dir in targets: sys.path.append(dir)    # add all subdirectories to python path
+    print(f"Path Hammer: subdirectories appended to python path")
 # ----- END PATH HAMMER ----- #
 
 # local-imports
-from hardware_interface.abstract_wrapper import AbstractWrapper as interface
+from abstract_wrapper import AbstractWrapper as interface
 
 
 # ----- PARENT CLASS ----- #
