3f02b6c96f8c92bcbeb75add64d80a838f13e933
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-08-27 11:13:35 -0400

    Added path hammer to calculations_factory.py

--- src/diagnostics/calculations/calculations_factory.py
+++ src/diagnostics/calculations/calculations_factory.py
@@ -3,7 +3,39 @@
 #   - add docstrings
 
 # built-in imports
+import sys
+import os
 from enum import Enum, unique
+
+# ----- PATH HAMMER ----- resolve absolute imports ----- #
+if __name__ == "__main__":  # execute snippet if current script was run directly
+    name = "Path Hammer"
+    print(f"{name}: resolving imports...")
+    
+    # HAMMER ARGUMENTS
+    num_dir = 1     # how many parent folders to reach /plasmetry/src
+    targets = [     # folders to be added to the python path
+        '\\abstract_layers',
+        '\\control',
+        '\\diagnostics',
+        '\\hardware_interface',
+        '\\user_interface',
+        '\\utils']
+    
+    # Locate absolute path to /plasmetry/src
+    print(f"{name}: locating plasmetry source path...")
+    parent = os.path.dirname(__file__)      # get current file's parent directory
+    for _ in range(num_dir):                # traverse directory upwards
+        parent = os.path.dirname(parent)    # get next parent
+    print(f"{name}: {parent}")  # expect absolute path to /plasmetry/src
+
+    # Append all target folders to python path
+    print(f"{name}: appending target paths...")
+    for subdir in targets:
+        target = parent + subdir
+        print(f"{name}: {subdir}")
+        sys.path.append(target)
+    print(f"{name}: complete.")
 
 # ----- LOCAL IMPORTS ----- #
 # PARAMETER EQUATIONS
