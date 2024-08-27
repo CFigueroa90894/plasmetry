cb848f6adcb0c174aac2325c259e95911b2d218a
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-08-26 20:17:37 -0400

    Changed AbstractWrapper's class methods to instance methods.

--- src/hardware_interface/abstract_wrapper.py
+++ src/hardware_interface/abstract_wrapper.py
@@ -10,26 +10,22 @@ class AbstractWrapper(ABC):
     """<...>"""
     
     # ----- ANALOG I/0 ----- #
-    @classmethod
     @abstractmethod
     def write_analog(cls, address:int, value:float) -> None:
         """<...>"""
         raise NotImplementedError
     
-    @classmethod
     @abstractmethod
     def read_analog(cls, address:int) -> float:
         """<...>"""
         raise NotImplementedError
     
     # ----- DIGITAL I/O ----- #
-    @classmethod
     @abstractmethod
     def write_digital(cls, address:int, level:bool) -> None:
         """<...>"""
         raise NotImplementedError
     
-    @classmethod
     @abstractmethod
     def read_digital(cls, address:int) -> bool:
         """<...>"""
