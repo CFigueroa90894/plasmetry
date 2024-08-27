8d6cdb4266d7a1d5a51f8cf278cef21057ad01c8
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-08-27 11:19:07 -0400

    Added path hammer to Base_TLP.py and updated absolute imports.

--- src/diagnostics/probe_objects/abstract_probes/Base_TLP.py
+++ src/diagnostics/probe_objects/abstract_probes/Base_TLP.py
@@ -3,18 +3,41 @@
 #   - add docstrings
 #   - add hardware interface objects
 
+# built-in imports
 import sys
 import os
 
-# add the src folder to the python path to locate local modules
-if __name__ == "__main__":
-    target = os.path.dirname(__file__)  # abstract probes
-    target = os.path.dirname(target)    # probe objects
-    target = os.path.dirname(target)    # diagnostics
-    target = os.path.dirname(target)    # src
-    sys.path.append(target)             # add src for absolute imports
+# ----- PATH HAMMER ----- resolve absolute imports ----- #
+if __name__ == "__main__":  # execute snippet if current script was run directly
+    name = "Path Hammer"
+    print(f"{name}: resolving imports...")
+    
+    # HAMMER ARGUMENTS
+    num_dir = 3     # how many parent folders to reach /plasmetry/src
+    targets = [     # folders to be added to the python path
+        '\\abstract_layers',
+        '\\control',
+        '\\diagnostics',
+        '\\hardware_interface',
+        '\\user_interface',
+        '\\utils']
+    
+    # Locate absolute path to /plasmetry/src
+    print(f"{name}: locating plasmetry source path...")
+    parent = os.path.dirname(__file__)      # get current file's parent directory
+    for _ in range(num_dir):                # traverse directory upwards
+        parent = os.path.dirname(parent)    # get next parent
+    print(f"{name}: {parent}")  # expect absolute path to /plasmetry/src
 
-from diagnostics.probe_objects.abstract_probes.Base_Probe import BaseProbe
+    # Append all target folders to python path
+    print(f"{name}: appending target paths...")
+    for subdir in targets:
+        target = parent + subdir
+        sys.path.append(target)
+    print(f"{name}: complete")
+
+# local imports
+from probe_objects.abstract_probes.Base_Probe import BaseProbe
 
 class BaseTLP(BaseProbe):
     "<...>"
