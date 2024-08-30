6f934a6a873a27cceaa214d7783f423f8c603540
Alberto <albertocases@e-3consulting.com>
2024-08-30 14:01:12 -0400

    update slp params, pep 8 formatting.
    One line exceeds 100, will discuss with team.

--- src/diagnostics/calculations/testing/parameters/slp_plasma_parameters.py
+++ src/diagnostics/calculations/testing/parameters/slp_plasma_parameters.py
@@ -1,14 +1,19 @@
 import numpy as np
-from global_parameters import get_debye_length, get_number_of_electrons, get_particle_density
 from scipy import signal
 
+from global_parameters import (
+    get_debye_length,
+    get_number_of_electrons,
+    get_particle_density
+)
 """TODO: get best way to yield ion saturation current"""
 
 # Storing the charge of the electron particle, since it shall be used for calculation
-electron_charge = 1.60217657e-19
+ELECTRON_CHARGE = 1.60217657e-19
 
 
 def filter_current(parameters):
+    
     # Extracted from Felix Cuadrado's code
     """Filters a signal using a butterworth digital filter. 
     
@@ -28,7 +33,9 @@ def filter_current(parameters):
 def get_floating_and_plasma_potential(parameters):
     
     
-    """This function yields two lists that contains the floating potential and plasma potential and their respective index within the filtered_current_list. 
+    """This function yields two lists that contains the floating potential and plasma potential
+    
+    and their respective index within the filtered_current_list. 
     
     The floating potential is defined as the voltage at which the current captured is 0.
     
@@ -39,75 +46,92 @@ def get_floating_and_plasma_potential(parameters):
     filtered_current_list = parameters['Filtered current'] 
     voltage_list = parameters['Bias']
 
-    # Storing the index of the floating potential and it's value
+    # Storing the index of the floating potential and its value
     parameters['Floating potential index'] = np.argmin(abs(filtered_current_list)) 
     parameters['Floating potential'] = voltage_list[parameters['Floating potential index']]
     
     
     """The plasma potential may be yielded from the value of the voltage where the 
 
-    minimum value of the second order derivate occurs
+    minimum value of the second order derivate occurs.
     """
     
     # Acquiring second order derivative
     first_derivative = np.gradient(filtered_current_list, voltage_list)
     second_derivative = np.gradient(first_derivative, voltage_list)
-    # Storing the index of the plasma potential and it's value
+    
+    # Storing the index of the plasma potential and its value
     parameters['Plasma potential index'] = np.argmin(second_derivative)
     parameters['Plasma potential'] = voltage_list[parameters['Plasma potential index']]
     
-    if parameters['Plasma potential']<= 0: 
-        return 'currents measured in this sweep are not valid, as they do not produce a sigmoid.'
+    # If the calculated plasma potential is negative,
+    # biases entered or data captured does not produce expected sigmoid.
+    if parameters['Plasma potential'] <= 0: 
+        return 'This sweep is not valid'
 
 
 def get_electron_saturation_current(parameters):    
     
-    """The electron saturation current yields from this function.
+    """The electron saturation current is yielded from this function.
     
     The electron saturation region commences at the plasma potential,
+    
     thus the current acquired at this point yields the value of the electron saturation current.
     """
     
-    filtered_current_list = parameters['Filtered current']
+    # Storing the plasma potential index
+    saturation_index = parameters['Plasma potential index']
     
-    # Storing the current acquired at the plasma potential 
-    parameters['Electron saturation current'] = filtered_current_list[parameters['Plasma potential index']]
+    # Storing the current acquired at the plasma potential, A.K.A the electron saturation current
+    parameters['Electron saturation current'] = parameters['Filtered current'][saturation_index]
 
 
 def get_electron_temperature(parameters):
 
     """Single Langmuir Probe electron temperature in both electron volts 
     
-    and  Joules is returned by the slp_electron_temperature function.
+    and  Joules is calculated by the slp_electron_temperature function.
+    """
     
-    Electron temperature may be yielded from the inverse value of the slope of the ln(I)-V values
+    
+    """Electron temperature is be yielded from the inverse value of the slope of the ln(I)-V values
     
     between the floating and plasma potential.
     
     Since the plasma potential and floating potential are yielded as approximations,
     
-    The starting and final point used to calculate the slope of the ln(I)-V graph were chosen between 
+    the starting and final point used to calculate the slope of the ln(I)-V graph were chosen 
     
-    the previously acquired plasma and floating potential.
+    between the acquired plasma and floating potential since these points shall be within the line
     
-    Since these points should be within the line between the floating potential and plasma potential of the ln(i)-v 
+    that is formed between the floating potential and plasma potential of the ln(I)-V graph,
+    
+    thus producing the expected slope.
     """
+    
+    # Storing the number of points between the plasma potential and floating potential.
+    points_number = parameters['Plasma potential index'] - parameters['Floating potential index']
+    
+    # The starting point used calculate the slope is the point with an index 1/4 of the way
+    # between the floating potential and plasma potential.
+    starting_index = int(np.ceil(points_number * 0.25)) + parameters['Floating potential index']
+    
+    # The final point used to calculate the slope is the point with an index 3/4 of the distance 
+    # between the floating potential and plasma potential. 
+    final_index= int(np.ceil(points_number * 0.75 )) + parameters['Floating potential index']
+    
    
-    # The final point is the point with an index 3/4 of the distance between the floating potential and plasma potential. 
-    final_index= int(np.ceil((parameters['Plasma potential index']  -  parameters['Floating potential index']) *0.75 ))+ parameters['Floating potential index']
-    # The starting point is the point with an index 1/4 of the way between the floating potential and plasma potential. 
-    starting_index = int(np.ceil((parameters['Plasma potential index']  -  parameters['Floating potential index'] )*0.25) )+ parameters['Floating potential index']
-   
-    # To calculate the slope of the ln(I)-V values between the floating and plasma potential, the numerator and denominator shall be acquired 
+    # To calculate the slope, the numerator and denominator shall be acquired.
     numerator_of_slope = np.log(parameters['Filtered current'][final_index]) - \
     np.log(abs(parameters['Filtered current'] [starting_index]))
     denominator_of_slope = parameters['Bias'][final_index] - parameters['Bias'][starting_index]
     
-    # Denominator/numerator is being performed since the inverse value of the slope yields the electron temperature in electron volts 
-    parameters['Electron temperature (eV)'] = denominator_of_slope/numerator_of_slope
+    # Denominator / numerator is being performed 
+    parameters['Electron temperature (eV)'] = denominator_of_slope / numerator_of_slope
     
-    # Multiplying the electron particle mass times the electron temperature in electron volts yields the electron temperature in Joules
-    parameters['Electron temperature (Joules)'] = parameters['Electron temperature (eV)'] * electron_charge
+    # Multiplying the electron particle mass times the electron temperature in electron volts
+    # yields the electron temperature in Joules
+    parameters['Electron temperature (Joules)'] = parameters['Electron temperature (eV)'] * ELECTRON_CHARGE
 
 
 def get_equations():
@@ -132,14 +156,18 @@ def get_equations():
     return list_of_references
 
 
-if __name__ == "__main__": 
-    ''' sample usage of the equations'''    
-    def LoadPreviousData():
-        import csv 
 
+''' sample usage of the equations'''    
+if __name__ == "__main__": 
+
+    
+    def LoadPreviousData():
+        
         """Function to load data from previous implementation. Code developed by Felix Cuadrado"""
-        with open('../testing scenarios/Feliz_A1 MirorSLP120200813T105858.csv', newline='') as csvfile:
-            dataReader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        
+        import csv as csv_library
+        with open('../testing scenarios/Feliz_A1 MirorSLP120200813T105858.csv', newline='') as csv:
+            dataReader = csv_library.reader(csv, delimiter=',', quotechar='|')
             next(dataReader)  # Skip the header row
             current = []
             voltageSLP = []
@@ -151,6 +179,7 @@ if __name__ == "__main__":
                     None    
             
             return voltageSLP, current
+    
     
     # Parameter dictionary, stores parameters
     parameters= {}
@@ -173,6 +202,3 @@ if __name__ == "__main__":
     for i in parameters.keys():
         if 'current' not in i and 'Bias' != i:
             print( i + ': ', parameters[i])
-    
-    
-    
