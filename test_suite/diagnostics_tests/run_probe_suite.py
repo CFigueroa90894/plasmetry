62af01d628a518389f19afbc549037309b7a4af0
Alberto <albertocases@e-3consulting.com>
2024-09-04 16:08:52 -0400

    Update run_probe_suite.py

--- test_suite/diagnostics_tests/run_probe_suite.py
+++ test_suite/diagnostics_tests/run_probe_suite.py
@@ -5,28 +5,6 @@ sys.path.insert(0, os.path.abspath('Probe test cases'))
 from SLPTestCases import SLPTestCases
     
 
-def LoadPreviousData():
-    
-    """Function to load data from previous implementation. Code developed by Felix Cuadrado"""
-    
-    import csv as csv_library
-    import numpy as np
-    
-    with open('initial data for test cases/Feliz_A1 MirorSLP120200813T105858.csv', newline='') as csv:
-        dataReader = csv_library.reader(csv, delimiter=',', quotechar='|')
-        next(dataReader)  # Skip the header row
-        current = []
-        voltageSLP = []
-        for row in dataReader:
-            try:
-                current = np.append(current, float(row[0]))
-                voltageSLP = np.append(voltageSLP, float(row[1]))
-            except:
-                None    
-        
-        return voltageSLP, current
-
-
 def run_test_suite(probe_test_cases):
     
     suite = unittest.TestSuite()
@@ -41,6 +19,30 @@ def run_test_suite(probe_test_cases):
 
 
 if __name__ == '__main__':
+    
+    """SLP sample run"""
+    
+    def LoadPreviousData():
+        
+        """Function to load data from previous implementation. Code developed by Felix Cuadrado"""
+        
+        import csv as csv_library
+        import numpy as np
+        
+        with open('initial data for test cases/Feliz_A1 MirorSLP120200813T105858.csv', newline='') as csv:
+            dataReader = csv_library.reader(csv, delimiter=',', quotechar='|')
+            next(dataReader)  # Skip the header row
+            current = []
+            voltageSLP = []
+            for row in dataReader:
+                try:
+                    current = np.append(current, float(row[0]))
+                    voltageSLP = np.append(voltageSLP, float(row[1]))
+                except:
+                    None    
+            
+            return voltageSLP, current
+
     
     # Storing bias and raw current lists from previous implementation
     bias, raw_current =  LoadPreviousData()
