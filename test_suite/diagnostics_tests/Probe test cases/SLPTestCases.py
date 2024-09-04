9d2909cb0c00d78d518b7f6cb6c0787f5bf7f316
Alberto <albertocases@e-3consulting.com>
2024-09-04 15:13:01 -0400

    Update SLPTestCases.py

--- test_suite/diagnostics_tests/Probe test cases/SLPTestCases.py
+++ test_suite/diagnostics_tests/Probe test cases/SLPTestCases.py
@@ -1,11 +1,12 @@
 from BaseCase import BaseCase
+import unittest
 import sys 
 import os
 sys.path.insert(0, os.path.abspath('parameters'))
 
 import slp_plasma_parameters
+import global_parameters
 
-import unittest
 
 class SLPTestCases(BaseCase):
 
@@ -13,9 +14,6 @@ class SLPTestCases(BaseCase):
         """Test the filter_current function"""
         slp_plasma_parameters.filter_current(self.parameters)
         self.assertIn('Filtered current', self.parameters)
-      
-    def test_get_particle_density(self):
-        print('aaa')
 
     def test_get_floating_and_plasma_potential(self):
         """Test the get_floating_and_plasma_potential function"""
@@ -40,7 +38,7 @@ class SLPTestCases(BaseCase):
         slp_plasma_parameters.get_electron_saturation_current(self.parameters)
         self.assertIn('Electron saturation current', self.parameters)
         self.assertEqual(self.parameters['Electron saturation current'], self.parameters['Filtered current'][self.parameters['Plasma potential index']])
-        self.assertGreaterEqual(self.parameters['Electron saturation current'], 0, "Electron saturation current should be non-negative")
+        self.assertGreater(self.parameters['Electron saturation current'], "Electron saturation current should be positive.")
 
     def test_get_electron_temperature(self):
         """Test the get_electron_temperature function"""
@@ -52,7 +50,18 @@ class SLPTestCases(BaseCase):
         self.assertIn('Electron temperature (Joules)', self.parameters)
         self.assertGreater(self.parameters['Electron temperature (eV)'], 0, "Electron temperature (eV) should be positive")
         self.assertGreater(self.parameters['Electron temperature (Joules)'], 0, "Electron temperature (Joules) should be positive")
-        
+    
+          
+    def test_get_particle_density(self):
+        """Test the get_particle_density global function"""
+        slp_plasma_parameters.filter_current(self.parameters)
+        slp_plasma_parameters.get_floating_and_plasma_potential(self.parameters)
+        slp_plasma_parameters.get_electron_saturation_current(self.parameters)
+        slp_plasma_parameters.get_electron_temperature(self.parameters)
+        slp_plasma_parameters.get_electron_temperature(self.parameters)
+        global_parameters.get_particle_density(self.parameters)
+        self.assertIn('Electron  density', self.parameters)
+
     def test_get_debye_length(self):
         print('aaa')
         
