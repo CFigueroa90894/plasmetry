bcb972c60505d387c5d56fe019389bb5eb416b07
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-09-06 00:40:38 -0400

    Removed function signature in abstract_diagnostics.py.

--- src/abstract_layers/abstract_diagnostics.py
+++ src/abstract_layers/abstract_diagnostics.py
@@ -7,17 +7,7 @@ from abc import ABCMeta, abstractmethod
 
 
 class AbstractDiagnostics(metaclass=ABCMeta):
-    @abstractmethod
-    def __init__(self,
-                 config_ref,            # ProtectedDictionary object containing user-settings
-                 sys_ref,               # ProtectedDictionary object containing system-settings
-                 status_flags,          # collection of Events to indicate system states
-                 command_flags,         # collection of Events to trigger actions
-                 hardware_wrapper_cls,  # wrapper class for hardware interface implementation
-                 real_time_param,       # ProtectedDicitonary object to store most recent calculated parameters
-                 results_buffer,        # Queue to return aggregated results to Control Layer
-                 args, **kwargs):
-        raise NotImplementedError("This function was not overloaded in the subclass!")
+    """<...>"""
     
     @abstractmethod
     def start_diagnostics(self):
