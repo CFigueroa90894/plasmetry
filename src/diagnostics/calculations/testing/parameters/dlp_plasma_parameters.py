28d6519b8f71e46f0a2dadd1a495f58602e51e4d
Alberto <albertocases@e-3consulting.com>
2024-09-09 14:22:43 -0400

    update slp & dlp, aggregated path hammer v2.4

--- src/diagnostics/calculations/testing/parameters/dlp_plasma_parameters.py
+++ src/diagnostics/calculations/testing/parameters/dlp_plasma_parameters.py
@@ -1,3 +1,20 @@
+import os
+import sys
+
+# ----- PATH HAMMER v2.4 ----- resolve absolute imports ----- #
+if __name__ == "__main__":  # execute snippet if current script was run directly 
+    num_dir = 4            # how many parent folders to reach /plasmetry/src
+
+    src_abs = os.path.abspath(os.path.dirname(__file__) + num_dir*'/..') # absolute path to plasmetry/src
+    print(f"Path Hammer: {src_abs}")
+    split = src_abs.split('\\')     # separate path into folders for validation
+    assert split[-2] == 'plasmetry' and split[-1] == 'src'  # validate correct top folder
+    
+    targets = [x[0] for x in os.walk(src_abs) if x[0].split('\\')[-1]!='__pycache__'] # get subdirs, exclude __pycache__
+    for dir in targets: sys.path.append(dir)    # add all subdirectories to python path
+   # print(f"Path Hammer: subdirectories appended to python path")
+# ----- END PATH HAMMER ----- #
+
 import numpy as np 
 from global_parameters import (
     filter_current,
