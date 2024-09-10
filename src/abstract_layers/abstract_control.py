e954c814a6c380e39d5371a4ce9382a4fd286e72
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-09-10 11:47:33 -0400

    Corrections in module's header docstring.

--- src/abstract_layers/abstract_control.py
+++ src/abstract_layers/abstract_control.py
@@ -1,9 +1,10 @@
-""" G3 - Plasma Devs
+"""G3 - Plasma Devs
 Layer 2 - Control Interface
     Defines the required public functionality for concrete implementations of the Control Layer.
 
 author: figueroa_90894@students.pupr.edu
 status: WIP
+    - confirm and agree with team on interface specifications
 """
 
 # built-in imports
