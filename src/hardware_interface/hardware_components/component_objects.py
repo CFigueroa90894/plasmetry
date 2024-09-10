9318f9003e333734349eb74431435c579967fe72
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-09-10 17:02:36 -0400

    Created component_objects.py, added docstring header and path hammer.

---
+++ src/hardware_interface/hardware_components/component_objects.py
@@ -0,0 +1,28 @@
+"""G3 - Plasma Devs
+Layer 4 - Hardware Interface - Component Objects
+    Defines classes for probe subcomponents, including basic sensors and amplifiers.
+
+author: figueroa_90894@students.pupt.edu
+status: WIP
+  - add docstrings
+  - finish classes
+"""
+
+# built-in imports
+import sys
+import os
+
+# ----- PATH HAMMER v2.7 ----- resolve absolute imports ----- #
+def path_hammer(num_dir:int, root_target:list[str], exclude:list[str], suffix:str="") -> None:
+    """Resolve absolute imports by recusring into subdirs and appending them to python path."""
+    src_abs = os.path.abspath(os.path.dirname(__file__) + num_dir*'/..' + suffix)
+    assert src_abs.split('\\')[-1*len(root_target):] == root_target   # validate correct top folder
+    
+    # get subdirs, exclude unwanted
+    dirs = [sub[0] for sub in os.walk(src_abs) if sub[0].split('\\')[-1] not in exclude]
+    for dir in dirs: sys.path.append(dir)    # add all subdirectories to python path
+    print(f"Path Hammer: {src_abs}")
+
+if __name__ == "__main__":  # execute path hammer if this script is run directly
+    path_hammer(2, ['plasmetry', 'src'], ['__pycache__'])  # hammer subdirs in plasmetry/src
+# ----- END PATH HAMMER ----- #
