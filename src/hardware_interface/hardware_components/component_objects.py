d4ac45cebca99bc2cc7bb2732a8bbc44c7be8392
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-09-10 18:09:38 -0400

    Implemented the HighVoltAmp class.

--- src/hardware_interface/hardware_components/component_objects.py
+++ src/hardware_interface/hardware_components/component_objects.py
@@ -30,6 +30,7 @@ if __name__ == "__main__":  # execute path hammer if this script is run directly
 
 # local imports
 from channel_objects import AnalogIn, AnalogOut, DigitalOut
+from range_map import high_to_low
 
 
 # ----- INPUT SUBCOMPONENTS ----- #
@@ -43,6 +44,10 @@ class VoltageSensor:
     Protected Methods:
         _division_read(): float - returns voltage divided by gain
         _no_division_read(): float - returns the voltage, without dividing by gain
+
+    Protected Attributes:
+        _input: AnalogIn - associated input channel
+        _gain: float - gain of associated diff amp
     """
 
     def __init__(self, gain:float, analog_in: AnalogIn):
@@ -56,7 +61,7 @@ class VoltageSensor:
         # Validate analog_in
         if not isinstance(analog_in, AnalogIn):
             err_msg = f"analog_in must be instance of AnalogIn!"
-            err_msg += f" Given {type(AnalogIn)}"
+            err_msg += f" Given {type(analog_in)}"
             raise TypeError(err_msg)
         else:
             self._input = analog_in
@@ -87,7 +92,19 @@ class VoltageSensor:
     
 # ----- OUTPUT SUBCOMPONENTS ----- #
 class BaseAmp:
-    """Parent class for concrete amplifier classes."""
+    """Parent class for amplifier subclasses.
+    
+    Public Methods:
+        __init__() - initialize the object
+
+    Protected Attributes:
+        _output: AnalogOut - the associated output channel
+        _dac_min: float - the minimum value expected by the amp's input
+        _dac_max: float - the maximum value expected by the amp's input
+        _amp_min: float - the minimum value produced at the amp's output
+        _amp_max: float - the maximum value produced at the amp's output
+
+    """
     def __init__(self, dac_range:dict, amp_range: dict, analog_out: AnalogOut):
         """Constructor for BaseAmp and its subclasses.
         
@@ -96,23 +113,61 @@ class BaseAmp:
             amp_range: dict - min and max of the amp's output
             analog_out: AnalogOut - output channel to the amplifier
         """
-        pass
+        
+        # Validate analog_out
+        if not isinstance(analog_out, AnalogOut):
+            err_msg = f"analog_out must be instance of AnalogOut!"
+            err_msg += f" Given {type(analog_out)}"
+            raise TypeError(err_msg)
+        else:
+            self._output = analog_out
+        
+        # Save mapping ranges
+        self._dac_min = dac_range['min']
+        self._dac_max = dac_range['max']
+        self._amp_min = amp_range['min']
+        self._amp_max = amp_range['max']
 
-class HVAmp(BaseAmp):
-    """<...>"""
+
+class HighVoltAmp(BaseAmp):
+    """Subclass of BaseAmp, associated with a high voltage amplifier.
+    
+    Public Methods:
+        __init__() - constructor inherited from parent
+        write(float) - produce the desired voltage at the HV-amp's output
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Constructor for HVAMP class, inherited from BaseAmp"""
+        super().__init__(*args, **kwargs)   # invoke parent constructor
+
+    def write(self, voltage: float):
+        """Sets the associated high voltage amplifier's output to the given value."""
+        low_volt = self._translate(voltage)     # translate desired HV-out to required LV-in
+        self._output.write(low_volt)            # output required LV to associated output channel
+
+    def _translate(self, desired_high_voltage):
+        """Obtain the required low voltage input to produce the desired high voltage output using
+        the amp's and dac's min and max values."""
+        # Translate using utils method and given ranges
+        low_volt = high_to_low(
+            high_val=desired_high_voltage,
+            low_min=self._dac_min,
+            low_max=self._dac_max,
+            high_min=self._amp_min,
+            high_max=self._amp_max
+        )
+        return low_volt
+
+
+class VoltageSweeper(HighVoltAmp):
+    """Subclass of BaseAmp, associated with a high voltage amplifier to perform voltage sweeps.
+    
+    The VoltageSweeper pre-computes the values of its voltage steps in order to save performance
+    during diagnostics, avoiding calls to translate desired high voltage to required low voltage.
+    """
     pass
 
-class BaseAmp:
-    """<...>"""
-    pass
-
-class VoltageSweeper(HVAmp):
-    """<...>"""
-    pass
-
-class BaseAmp:
-    """<...>"""
-    pass
 
 class RelaySet:
     """<...>"""
