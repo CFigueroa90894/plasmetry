044eb6efb7d348ff7ac14a8706b9d72bd8d5daff
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-09-10 16:53:07 -0400

    Created folder hardware_wrappers and moved relevant files.

--- src/hardware_interface/hardware_wrappers/concrete_wrappers/daqc2plate_wrapper.py
+++ src/hardware_interface/hardware_wrappers/concrete_wrappers/daqc2plate_wrapper.py
@@ -13,7 +13,7 @@ import os
 
 # ----- PATH HAMMER v2.4 ----- resolve absolute imports ----- #
 if __name__ == "__main__":  # execute snippet if current script was run directly 
-    num_dir = 2             # how many parent folders to reach /plasmetry/src
+    num_dir = 3             # how many parent folders to reach /plasmetry/src
 
     src_abs = os.path.abspath(os.path.dirname(__file__) + num_dir*'/..') # absolute path to plasmetry/src
     print(f"Path Hammer: {src_abs}")
