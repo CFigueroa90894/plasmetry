034719d949f2f95a85ca9b625c5858db8006dc01
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-09-11 01:50:32 -0400

    Added docstrings to ComponentFactory and updated to do list accordingly.

--- src/hardware_interface/hardware_components/component_factory.py
+++ src/hardware_interface/hardware_components/component_factory.py
@@ -4,7 +4,6 @@ Layer 4 - Hardware Interface - Component Factory
 
 author: figueroa_90894@students.pupt.edu
 status: WIP
-  - add docstrings
   - validate with team
 """
 
@@ -46,20 +45,32 @@ class COMP(Enum):
 
 
 class ComponentFactory:
-    """<...>
+    """The ComponentFactory generates hardware component objects to control their associated
+    physical counterparts.
     
     Class Attributes:
         + ID: Enum - component types supported by this factory
-        - CHN: Enum - channel types used by this factory
+        - __CHN: Enum - channel types used by this factory
 
     Instance Attributes:
         - __channel: ChannelFactory - generates channel objects that make up component objects
+
+    Methods:
+        + __init__() - initialize the component factory
+        + make() - return generated hardware component object
+        # _make_hv_amp() - instantiate a HighVoltAmp object
+        # _make_volt_sweeper() - instantiate a VoltagerSweeper object
+        # _make_volt_sensor() - instantiate a VoltageSensor object
+        # _make_relay() - instantiate a RelaySet object
+        - __make_analog_out() - instantiate an analog output channel
+        - __make_analog_in() - instantiate an analog input channel
+        - __make_digital_out() - instantiate a digital output channel
     """
     ID:COMP = COMP  # package component factory's valid IDs as class attributes
     __CHN = None    # placeholder for channel factory's supported objects
 
     def __init__(self, channel_factory):
-        """<...>
+        """Instantiates a ComponentFactory object with the given ChannelFactory object.
         
         Arguments:
             channel_factory - an instantiated ChannelFactory object
@@ -71,40 +82,43 @@ class ComponentFactory:
         """<...>"""
         # Select component to instantiate
         match type:
-            case COMP.HVAMP:
+            case COMP.HVAMP:  # HighVoltAmp
                 return self._make_hv_amp(*args, **kwargs)
             
-            case COMP.SWEEP:
+            case COMP.SWEEP:  # VoltageSweeper
                 return self._make_volt_sweeper(*args, **kwargs)
             
-            case COMP.VSENS:
+            case COMP.VSENS:  # VoltageSensor
                 return self._make_volt_sensor(*args, **kwargs)
             
-            case COMP.RLSET:
+            case COMP.RLSET:  # RelaySet
                 return self._make_relay(*args, **kwargs)
 
 
     # ----- Component Instantiators ----- #
     def _make_hv_amp(self, address:int, *args, **kwargs):
-        """<...>"""
+        """Return an instatiated HighVoltAmp object by making an analog output channel and
+        forwarding other given arguments."""
         output_channel = self.__make_analog_out(address)
         return HighVoltAmp(analog_out=output_channel, *args, **kwargs)
 
 
     def _make_volt_sweeper(self, address:int, *args, **kwargs):
-        """<...>"""
+        """Return an instatiated VoltagerSweeper object by making an analog output channel and
+        forwarding other given arguments."""
         output_channel = self.__make_analog_out(address)
         return VoltageSweeper(analog_out=output_channel, *args, **kwargs)
 
 
     def _make_volt_sensor(self, address:int, *args, **kwargs):
-        """<...>"""
+        """Return an instatiated VoltageSensor object by making an analog input channel and
+        forwarding other given arguments."""
         input_channel = self.__make_analog_in(address)
         return VoltageSensor(analog_in=input_channel, *args, **kwargs)
 
 
     def _make_relay(self, address:Tuple[int], *args, **kwargs):
-        """<...>"""
+        """Return an instatiated RelaySet by iteratively generating digital output channels."""
         output_channels = []    # aggregate all digital output channels
         
         # instantiate individual digital output channels
@@ -117,16 +131,16 @@ class ComponentFactory:
 
     # ----- Channel Instantiators ----- #
     def __make_analog_out(self, address:int):
-        """<...>"""
+        """Return an analog output channel created by the channel factory."""
         return self.__channel.make(address, self.__CHN.AO)
     
 
     def __make_analog_in(self, address:int):
-        """<...>"""
+        """Return an analog input channel created by the channel factory."""
         return self.__channel.make(address, self.__CHN.AI)
 
 
     def __make_digital_out(self, address:int):
-        """<...>"""
+        """Return a digital output channel created by the channel factory."""
         return self.__channel.make(address, self.__CHN.DO)
 
