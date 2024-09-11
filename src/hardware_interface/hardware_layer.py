e55ab4ae1079c0809bb4877ec3af78a65b73e643
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-09-11 02:36:45 -0400

    Corrected load for channel_factory_cls. Added some docstrings. Added debug method _get_subcomponents. Updated to do list.

--- src/hardware_interface/hardware_layer.py
+++ src/hardware_interface/hardware_layer.py
@@ -6,6 +6,8 @@ Layer 4 - Hardware Interface - Concrete Implementation
 author: figueroa_90894@students.pupt.edu
 status: WIP
   - add docstrings
+  - implement reset_components() method
+  - validate with team
 """
 
 # built-in imports
@@ -27,22 +29,25 @@ if __name__ == "__main__":  # execute path hammer if this script is run directly
     path_hammer(1, ['plasmetry', 'src'], ['__pycache__'])
 # ----- END PATH HAMMER ----- #
 
+
 # local imports
 from abstract_hardware import AbstractHardware
 
 
-# class HardwareLayer(AbstractHardware):
+# TO DO
 class HardwareLayer(AbstractHardware):
     """<...>"""
+
+    # Default subcomponent module names
     hardware_wrapper_modname = 'daqc2plate_wrapper'
     channel_factory_modname = 'channel_factory'
     component_factory_modname = 'component_factory'
 
     def __init__(self):
         """<...>"""
-        # load layer subcomponents
+        # load subcomponents
         hardware_wrapper_cls = __import__(self.hardware_wrapper_modname).wrapper
-        channel_factory_cls = __import__(self.hardware_wrapper_modname).ChannelFactory
+        channel_factory_cls = __import__(self.channel_factory_modname).ChannelFactory
         component_factory_cls = __import__(self.component_factory_modname).ComponentFactory
 
         # assemble subcomponents
@@ -51,14 +56,25 @@ class HardwareLayer(AbstractHardware):
         self.__component = component_factory_cls(channel_factory=self.__channel)
 
     def get_component_factory(self):
-        """<...>"""
+        """Return the instantiated ComponentFactory."""
         return self.__component
     
     def get_channel_factory(self):
-        """<...>"""
+        """Return the instantiated ChannelFactory."""
         return self.__channel
     
+    # TO DO
     def reset_components(self):
         """<...>"""
         raise NotImplementedError
+    
+    def _get_subcomponents(self):
+        """Return info about the instantiated layer's subcomponents.
+        
+        Used for debugging system integration.
+        """
+        sub = [('Hardware Wrapper', str(self.__wrapper)),
+               ('Channel Factory', str(self.__channel)),
+               ('Component Factory', str(self.__component))]
+        return sub
 
