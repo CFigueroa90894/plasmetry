431652e6ab0b4d32e649343e271789ab0af2c562
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-09-11 14:12:37 -0400

    Implemented constructor for DiagnosticLayer class.

--- src/diagnostics/diagnostics_layer.py
+++ src/diagnostics/diagnostics_layer.py
@@ -14,6 +14,8 @@ status: WIP
 import sys
 import os
 
+from queue import Queue
+
 # ----- PATH HAMMER v2.7 ----- resolve absolute imports ----- #
 def path_hammer(num_dir:int, root_target:list[str], exclude:list[str], suffix:str="") -> None:
     """Resolve absolute imports by recusring into subdirs and appending them to the python path."""
@@ -39,5 +41,82 @@ from abstract_layers.abstract_diagnostics import AbstractDiagnostics
 class DiagnosticsLayer(AbstractDiagnostics):
     """<...>"""
 
-    pass
+    # Default subcomponent module names
+    calculations_factory_modname = 'calculations_factory'
+    probe_factory_modname = 'probe_factory'
+    probe_operation_modname = 'probe_operation'
+
+    # Default lower layer
+    hardware_layer_modname = 'hardware_layer'
+
+
+    def __init__(self, 
+                 status_flags, 
+                 command_flags,
+                 results_buffer:Queue=None,
+                 real_time_param:dict=None,
+        ):
+        """<...>"""
+        # --- Save Arguments --- #
+        """
+        NOTE: I'm wishing there was a way to instantiate flags with defaults without
+        needing to violate the principle of not importing modules from other layers.
+
+        Perhaps its for the best, we might not realize the layer is using different
+        flags, making debugging layer integrations harder.
+        """
+        self.__status = status_flags     # system state indicators
+        self.__command = command_flags   # action triggers
+
+        # validate results_buffer
+        if results_buffer is not None:
+            self.__result_buff = results_buffer
+        else:
+            self.__result_buff = Queue() # create default Queue
+
+        # validate real_time_param
+        if real_time_param is not None:
+            self.__real_time_param = real_time_param
+        else:
+            self.__real_time_param = {}  # create default dictionary
+
+        # ----- LOAD LAYER COMPONENTS ----- #
+        # load subcomponent modules
+        calculations_factory_mod = __import__(self.calculations_factory_modname)
+        probe_factory_mod = __import__(self.probe_factory_modname)
+        probe_operation_mod = __import__(self.probe_operation_modname)
+
+        # load subcomponent classes
+        calc_fac_cls = calculations_factory_mod.CalculationsFactory
+        probe_fac_cls = probe_factory_mod.ProbeFactory
+        probe_op_cls = probe_operation_mod.ProbeOperation
+
+        # load lower layer
+        hardware_layer_mod = __import__(self.hardware_layer_modname)
+        hardware_layer_cls = hardware_layer_mod.HardwareLayer
+
+        # ---- LAYER ASSEMBLY ---- #
+        # lower layer
+        self.__hardware = hardware_layer_cls()  # lower layer interface
+        self.__comp_fac = self.__hardware.get_component_factory() # hardware component factory
+
+        # calculations factory subcomponent
+        self.__calc_fac = calc_fac_cls      # calculations factory is not instantiable
+        
+        # probe factory subcomponent
+        self.__probe_fac = probe_fac_cls(
+            status_flags=self.__status,
+            command_flags=self.__command,
+            hardware_factory=self.__comp_fac,
+            calculations_factory=self.__calc_fac
+        )
+
+        # probe operation subcomponent
+        self.__probe_op = probe_op_cls(
+            status_flags=self.__status,
+            command_flags=self.__command,
+            results_buffer=self.__result_buff,
+            real_time_param=self.__real_time_param,
+            probe_factory=self.__probe_fac
+        )
 
