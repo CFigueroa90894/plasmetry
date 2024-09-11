6c848001a4406e6796015d79ecebcf52166bf813
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-09-11 13:24:10 -0400

    Started work on a boundary for the diagnostic layer in order to separate layer logic from probe control logic in the ProbeOperation class.

---
+++ src/diagnostics/diagnostics_layer.py
@@ -0,0 +1,43 @@
+""" G3 - Plasma Devs
+Layer 3 - Diagnostics - Concrete Implementation
+    Provides the main implementation for the Diagnostics Layer, assembling its subcomponents and
+    exposing the layer's public functionality.
+
+author: figueroa_90894@students.pupr.edu
+status: WIP
+    - add docstrings
+    - remove relative import (added so docstrings are displayed)
+    - validate with team
+
+"""
+# built-in imports
+import sys
+import os
+
+# ----- PATH HAMMER v2.7 ----- resolve absolute imports ----- #
+def path_hammer(num_dir:int, root_target:list[str], exclude:list[str], suffix:str="") -> None:
+    """Resolve absolute imports by recusring into subdirs and appending them to the python path."""
+    src_abs = os.path.abspath(os.path.dirname(__file__) + num_dir*'/..' + suffix)
+    assert src_abs.split('\\')[-1*len(root_target):] == root_target   # validate correct top folder
+    
+    # get subdirs, exclude unwanted
+    dirs = [sub[0] for sub in os.walk(src_abs) if sub[0].split('\\')[-1] not in exclude]
+    for dir in dirs: sys.path.append(dir)    # add all subdirectories to python path
+    print(f"Path Hammer: {src_abs}")
+
+if __name__ == "__main__":  # execute path hammer if this script is run directly
+    path_hammer(1, ['plasmetry', 'src'], ['__pycache__'])  # hammer subdirs in plasmetry/src
+# ----- END PATH HAMMER ----- #
+
+
+# TO DO - remove relative imports
+# local imports
+from abstract_layers.abstract_diagnostics import AbstractDiagnostics
+
+
+# TO TO
+class DiagnosticsLayer(AbstractDiagnostics):
+    """<...>"""
+
+    pass
+
