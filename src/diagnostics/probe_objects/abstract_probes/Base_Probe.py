3ee6fd47f6ee89ce9880730755e5a1bab37b499b
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-09-11 16:02:17 -0400

    Updated BaseProbe's constructor to match hardware interface changes.

--- src/diagnostics/probe_objects/abstract_probes/Base_Probe.py
+++ src/diagnostics/probe_objects/abstract_probes/Base_Probe.py
@@ -30,49 +30,51 @@ from base_thread import BaseThread
 class BaseProbe(BaseThread, metaclass=ABCMeta):
     """The top-level, abstract class for all probe implementations.
     Includes initialization for flags, data buffer, equations, config, and data buffer.
-    Defines abstract methods run(), and _graceful_exit()."""
+    Defines abstract methods run(), and _graceful_exit().
+    
+    Attributes:
+        + probe_id - identifier for the probe's type
+        + sys_ref: dict - reference to system settings
+        + config_ref: dict - reference to user settings
+        + status_flags - state indicators
+        + command_flags - action triggers
+        + data_buff: Queue - buffer to return data samples
+        + sampling_rate: int - samples to obtain per second (Hz)
+        + relay_set: RelaySet - collection of relays
+
+    Methods:
+        + __init__() - initialize the object, called by subclasses
+        + run() - perform data acquistion, override in subclasses
+        # _graceful_exit() - complete pending actions and temrinate, override in subclasses
+    """
     def __init__(self,
                  probe_id,
                  sys_ref:dict,
                  config_ref:dict,
-                 shutdown:Event,
-                 diagnose:Event,
-                 operating:Event,
+                 status_flags,
+                 command_flags,
                  equations:list,
                  data_buff:Queue,
                  sampling_rate:int,
-                 relay_address:int,
-                 hardware_factory_obj,
+                 relay_set:int
                  ):
-        
-        # SIGNAL FLAGS - Set externally, indicates an action this object must perform.
-        self.shutdown = shutdown        # graceful system-wide shutdown, begin exit script
-        self.diagnose = diagnose        # diagnostics must be performed
-        
-        # STATUS FLAGS - Set internally, notifies other subcomponents of this objects state.
-        # !!! HIGH VOLTAGE INDICATOR !!!
-        # Set this BEFORE enabling the relay
-        # Clear AFTER confirming the relay is disabled
-        self.operating = operating      # indicate that diagnostics are being performed
-        
+        """<...>"""
         # PROBE INFO
         self.id = probe_id                  # identifier for testing and validation
         self.sys_ref = sys_ref              # dictionary with system settings
         self.config_ref = config_ref        # dictionary with user settings
+
+        # SYSTEM FLAGS
+        self.status_flags = status_flags    # system state indicators
+        self.command_flags = command_flags  # action triggers
+        
+        # PROBE OPERATION
         self.equations = equations  # list of callables to calculate plasma parameters
         self.data_buff = data_buff  # thread-safe queue, pass data samples to probe operation
         self.sampling_rate = sampling_rate      # samples to obtain per second, Hertz (Hz)
 
-        # HARDWARE FACTORY SETUP
-        self.hard = hardware_factory_obj
-        self.HW = hardware_factory_obj.IDs 
-        
-        # pack arguments
-        relay_args = {"address": relay_address,
-                      "type": self.HW.DO}
-        
         # PROBE SUBCOMPONENTS
-        self._relay = self.hard.make(**relay_args)      # turn the probe circuits on or off
+        self.relay_set = relay_set          # collection of relays that select probe operating mode
     
     @abstractmethod
     def run(self) -> None:
