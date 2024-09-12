70b146a236de01e22b70b116428a79c8b59c08ed
Alberto <albertocases@e-3consulting.com>
2024-09-12 09:46:55 -0400

    data_formatting parameters copy for testing

---
+++ src/diagnostics/parameters/dlp_plasma_parameters.py
@@ -0,0 +1,159 @@
+import os
+import sys
+
+# ----- PATH HAMMER v2.4 ----- resolve absolute imports ----- #
+if __name__ == "__main__":  # execute snippet if current script was run directly 
+    num_dir = 4            # how many parent folders to reach /plasmetry/src
+
+    src_abs = os.path.abspath(os.path.dirname(__file__) + num_dir*'/..') # absolute path to plasmetry/src
+    print(f"Path Hammer: {src_abs}")
+    split = src_abs.split('\\')     # separate path into folders for validation
+    assert split[-2] == 'plasmetry' and split[-1] == 'src'  # validate correct top folder
+    
+    targets = [x[0] for x in os.walk(src_abs) if x[0].split('\\')[-1]!='__pycache__'] # get subdirs, exclude __pycache__
+    for dir in targets: sys.path.append(dir)    # add all subdirectories to python path
+   # print(f"Path Hammer: subdirectories appended to python path")
+# ----- END PATH HAMMER ----- #
+
+import numpy as np 
+from global_parameters import (
+    filter_current,
+    get_debye_length,
+    get_number_of_electrons,
+    get_display_parameters
+)
+
+
+def get_ion_saturation_current(parameters):
+    
+    """**TODO: find a better way to obtain ion saturation current.**
+    
+    The ion saturation current is yielded from this function.
+    
+    NOTE: This is a simple and crude way to obtain the value.
+    """
+    
+    # Storing the ion saturation current.
+    parameters['Ion saturation current'] = np.min(parameters['Filtered current'] )
+   
+    
+def get_electron_temperature( parameters):
+    
+    """This function calculates the electron temperature in both Joules and electron volts.
+    
+    The value of  the derivative of the I-V values where the voltage applied is 0 is used 
+    
+    to calculate electron temperature, yet it is possible that the voltage applied is never 0, 
+    
+    thus the value closet to 0 shall be used. 
+    """
+    # Storing the charge of the electron particle, since it shall be used for calculation
+    ELECTRON_CHARGE = 1.60217657e-19
+    
+    # Storing parameters used for calculations
+    filtered_current_list = parameters['Filtered current'] 
+    voltage_list =  parameters['Bias 1'] 
+    ion_saturation_current = parameters['Ion saturation current'] 
+    
+    # Storing the index where the voltage is closest to 0.
+    voltage_at_zero_index = np.argmin(abs(voltage_list))
+    
+    # Storing the derivative of the I-V values.
+    I_V_derivative = np.gradient(filtered_current_list, voltage_list )
+    
+    denominator_of_equation = 2 * I_V_derivative[voltage_at_zero_index]
+    # Calculating electron temperature in electron volts. 
+    parameters['Electron temperature (eV)'] = abs(ion_saturation_current / denominator_of_equation)
+    
+    # Calculating electron temperature in Joules.
+    parameters['Electron temperature (Joules)'] = parameters['Electron temperature (eV)'] * ELECTRON_CHARGE
+
+
+def get_electron_density(parameters):
+    
+    """This function yields the electron density in Kilograms per cubic meter. """ 
+    
+    # Storing the charge of the electron particle, since it shall be used for calculation
+    ELECTRON_CHARGE = 1.60217657e-19
+    
+    # Configuration object stored, in order to get 'Probe Area' and 'Ion mass'
+    config_object = parameters['config_ref']
+    probe_area = config_object['Probe area']
+    ion_mass = config_object['Particle mass']
+    
+    # Acquiring electron density 
+    square_root_term =  np.sqrt(ion_mass / parameters['Electron temperature (Joules)'])
+    parameters['Electron density'] = abs(parameters['Ion saturation current'] / \
+                                        (ELECTRON_CHARGE *  probe_area) * \
+                                        (square_root_term * np.exp(0.5)))
+
+
+def get_equations():
+    
+    """This function returns a reference to the equations """
+    
+    # List containing the references
+    list_of_references = []
+    
+    # Local parameters
+    list_of_references.append(filter_current)
+    list_of_references.append(get_ion_saturation_current)
+    list_of_references.append(get_electron_temperature)
+    list_of_references.append(get_electron_density)
+    
+    # Global parameters
+    list_of_references.append(get_debye_length)
+    list_of_references.append(get_number_of_electrons)
+    list_of_references.append(get_display_parameters)
+    return list_of_references
+
+
+""" Sample usage of the equations. Requires protected_dictionary to be loaded in memory."""    
+if __name__ == "__main__": 
+
+    
+    def LoadPreviousData():
+        
+        """Function to load data from previous implementation. Code developed by Felix Cuadrado"""
+        
+        import csv as csv_library
+        
+        with open('../testing scenarios/Feliz_A1 MirorSLP120200813T105858.csv', newline='') as csv:
+            dataReader = csv_library.reader(csv, delimiter=',', quotechar='|')
+            next(dataReader)  # Skip the header row
+            current = []
+            voltageSLP = []
+            for row in dataReader:
+                try:
+                    current = np.append(current, float(row[0]))
+                    voltageSLP = np.append(voltageSLP, float(row[1]))
+                except:
+                    None    
+            
+            return voltageSLP, current
+    
+    # Parameter dictionary, stores parameters
+    parameters= {}
+    
+    # Storing bias and raw current lists from previous implementation
+    parameters['Bias 1'], parameters['Raw voltage 1'] =  LoadPreviousData()
+    
+    # Storing Probe area of a previous implementation, and ion mass in kg of argon, 
+    # simulating config values
+    parameters['config_ref'] = {'Probe area' : 30.3858e-06, 'Particle mass': 6.629e-26, 'Shunt 1': 1}
+    
+    
+    # Running each equation
+    list_of_equations = get_equations()
+    
+    for i in list_of_equations[:len(list_of_equations)-1]:
+        i(parameters)
+    
+    # Requires protected_dictionary to be loaded in memory
+    parameters_to_display = list_of_equations[-1](parameters)
+    
+    keys = parameters_to_display.keys()
+    
+    for i in keys: 
+        print(i, ':', parameters_to_display[i])
+        
---
+++ src/diagnostics/parameters/tlv_plasma_parameters.py
@@ -0,0 +1,169 @@
+import numpy as np 
+from global_parameters import get_display_parameters
+
+
+
+# Storing the charge of the electron particle, since it shall be used for calculation
+ELECTRON_CHARGE = 1.60217657e-19
+ 
+NUMBER_OF_ITERATIONS = 100
+
+TOLERANCE = 1e-5
+
+
+def filter_current(parameters):
+    
+    parameters['Potential difference'] = np.sum(parameters['Raw voltage 1']) / len(parameters['Raw voltage 1'])
+    raw_current = np.sum(parameters['Raw voltage 2']) / len(parameters['Raw voltage 2'])
+    parameters['Filtered current'] = raw_current / parameters['config_ref']['Shunt 1']
+    
+def iteration(potential_difference, bias, estimated_guess):
+    
+    """This function performs the function and derivative calculation process in each iteration.
+    
+       Returns the output of the expressions."""
+    
+    # Declaring limit to avoid overflow
+    LIMIT = 500  
+    
+    # First exponential term, shall be used for function and derivative calculation
+    first_exp_term = 2 * np.exp(np.clip(potential_difference * estimated_guess, None, LIMIT))
+    # Second exponential term,  shall be used for function and derivative calculation
+    second_exp_term = np.exp(np.clip(bias * estimated_guess, None, LIMIT))
+    
+    # Storing the function, to be used for the next estimated guess calculation
+    function_output =  first_exp_term - second_exp_term - 1     
+    # Storing the prime value, to be used for the next estimated guess calculation
+    derivative_output = potential_difference * first_exp_term - bias *  second_exp_term    
+    
+    # If the derivative calculation output is 0, returning 0 to trigger TypeError,
+    # since the output will be used as a denominator.
+    if derivative_output == 0:
+        return 0
+    
+    return function_output, derivative_output
+
+
+def get_electron_temperature(parameters):
+
+    
+    """This function calculates electron temperature in electron volts and Joules.
+    
+    The Newton-Raphson method has been deployed to calculte the inverse value of the temperature.
+    
+    The loop runs 100 iterations unless a value has been approximated, with a tolerance of 1e-5.
+    """
+    
+    potential_difference = parameters['Potential difference']
+    
+    bias =  parameters['Bias 1']
+    
+    # Storing the counter, shall be used to know the number of iterations
+    counter = 0
+    
+    # Variable storing the previous guess at the beginning of each iteration
+    previous_guess = 0
+   
+    # Storing initial guess for Newton-Raphson approximation iterations
+    estimated_guess = np.log(2) / (bias - potential_difference)
+    
+    # The Newton-Raphson approximation iterations occur in this while loop
+    while abs(estimated_guess - previous_guess) > TOLERANCE and counter < NUMBER_OF_ITERATIONS:
+        
+        # Storing previous guess, to compare with the final value of each iteration
+        previous_guess = estimated_guess
+        
+        # Try/except clause to verify if the derivative is not 0.
+        # If derivative == 0, exiting function
+        try:
+            
+            function_output, derivative_output = iteration(potential_difference, 
+                                                           bias,
+                                                           estimated_guess)
+        except TypeError:
+            return 'No Solution Found.'
+        
+        # Storing the next estimated value
+        estimated_guess = (estimated_guess - function_output / derivative_output)
+        
+        counter +=1
+        
+    if counter ==NUMBER_OF_ITERATIONS:
+        
+        return f'After {counter} iterations, no accurate value has been yielded.'
+    
+    # Storing the electron temperature in eV
+    parameters['Electron temperature (eV)'] = 1/estimated_guess
+    
+    # Storing the electron temperature in Joules
+    parameters['Electron temperature (Joules)'] = ELECTRON_CHARGE *  parameters['Electron temperature (eV)']
+
+
+def get_electron_density(parameters):
+    
+    """This equation yields electron density in particles per cubic meter."""
+    
+    # Configuration object stored, in order to get 'Probe Area'
+    config_object = parameters['config_ref']
+    probe_area = config_object['Probe area']
+    ion_mass =  config_object['Particle mass']
+    
+    # Storing the electron temperature in Joules, to be used in calculations
+    electron_temperature =  parameters['Electron temperature (Joules)']
+    
+    # Exponential term found in equation
+    exponential_term=  np.exp(abs(ELECTRON_CHARGE *  parameters['Potential difference'] / \
+                                  electron_temperature))
+                                 
+    # Since the density formula is composed of a division, yielding the numerator and denominator
+    numerator_of_equation= abs( parameters['Filtered current']  * exponential_term)
+    
+    denominator_of_equation = abs(0.61 * probe_area * ELECTRON_CHARGE * \
+                                  np.sqrt(electron_temperature / ion_mass) * \
+                                 (1 - exponential_term))
+    
+    # Storing electron density
+    parameters['Electron density'] = numerator_of_equation / denominator_of_equation
+    
+
+def get_equations():
+    
+    """This function returns a reference to the equations"""
+    
+    list_of_references = []
+    list_of_references.append(filter_current)
+    list_of_references.append(get_electron_temperature)
+    list_of_references.append(get_electron_density)
+    list_of_references.append(get_display_parameters)
+
+    return list_of_references
+
+
+""" sample usage of the equations"""    
+if __name__ == "__main__": 
+
+    
+    # Parameter dictionary, stores parameters
+    parameters= {}
+    
+    # Storing bias, measured current, and measured voltage to test the implementation.
+    parameters['Bias 1'], parameters['Potential difference'] =  40, 32
+    parameters['Raw voltage 1']= [34.4 for i in range(1,10)]
+    parameters['Raw voltage 2'] =  [0.005176711239483604 for i in range(1,10)]
+    # Storing Probe area of a previous implementation, and ion mass of Argon in kg, simulating config values
+    parameters['config_ref'] = {'Probe area' : 30.3858e-06, 'Particle mass':  6.629e-26, 'Shunt 1': 1}
+
+    
+    # Running each equation
+    list_of_equations = get_equations()
+   
+    for i in list_of_equations[:len(list_of_equations)-1]:
+        i(parameters)
+    
+    # Requires protected_dictionary to be loaded in memory
+    parameters_to_display = list_of_equations[-1](parameters)
+    
+    keys = parameters_to_display.keys()
+    
+    for i in keys: 
+        print(i, ':', parameters_to_display[i])
