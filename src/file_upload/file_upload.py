03fff9dacb0e4c1f71bf4f9363dc9cded5fab342
Alberto <albertocases@e-3consulting.com>
2024-09-12 14:13:05 -0400

    Update file_upload.py

--- src/file_upload/file_upload.py
+++ src/file_upload/file_upload.py
@@ -1,4 +1,5 @@
 import csv
+from data_formating import process_data
 
 class FileUpload:
     
@@ -8,7 +9,7 @@ class FileUpload:
         
         if unformatted_data:
             # Storing unformatted parameters dictionary
-            self.process_data(unformatted_data)
+            process_data(unformatted_data)
             
         self.local_path = local_path
         self.offsite_credentials = credentials_path
@@ -17,33 +18,18 @@ class FileUpload:
         
         """"""
         # Storing unformatted parameters dictionary
-        self.process_data(parameters)
+        process_data(parameters)
     
-    def is_tlp(self, unformatted_data):
+    def local_file_write(self, data):
         
         """"""
-        
-        if 'Bias 2' in unformatted_data:
-            return True
-        else:
-            return None
-            
-    def process_data(self, unformatted_data):
-        
-        """"""
-        self.sweep_data = []
-        self.calculated_parameters = []
-        
-        for experiment_run in unformatted_data:
-            if not self.is_tlp():
-               self.sweep_data.append({'Bias' : experiment_run['Bias 1'], \
-                                       'Raw Signal' : experiment_run['Raw voltage 1'],\
-                                       'Filtered current': experiment_run['Filtered current']})
-               del experiment_run['Bias 1']
-               del experiment_run['Raw voltage 1']
-               del experiment_run['Filtered current']
-            self.calculated_parameters.append(experiment_run)
-            
+
+        keys = list(data[0].keys())
+        with open(self.local_path, "w", newline='') as csv_file:
+            dict_writer= csv.DictWriter(csv_file, keys)
+            dict_writer.writeheader()
+            dict_writer.writerows(data)
+    
     def local_upload(self, local_path_name):
         
         """"""
@@ -54,15 +40,7 @@ class FileUpload:
         if  self.sweep_data: 
             self.local_file_write(self.sweep_data)
         
-    def local_file_write(self, data):
-        
-        """"""
-
-        keys = list(data[0].keys())
-        with open(self.local_path, "w", newline='') as csv_file:
-            dict_writer= csv.DictWriter(csv_file, keys)
-            dict_writer.writeheader()
-            dict_writer.writerows(data)
+    
             
     
 
