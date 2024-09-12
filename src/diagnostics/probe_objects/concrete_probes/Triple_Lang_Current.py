4bd12fbba8d32b749ee96e5ff8f77a9bfd618fe6
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-09-12 00:12:04 -0400

    Modified path hammer comments to stay under 100 chars per line.

--- src/diagnostics/probe_objects/concrete_probes/Triple_Lang_Current.py
+++ src/diagnostics/probe_objects/concrete_probes/Triple_Lang_Current.py
@@ -12,12 +12,14 @@ import os
 if __name__ == "__main__":  # execute snippet if current script was run directly 
     num_dir = 3             # how many parent folders to reach /plasmetry/src
 
-    src_abs = os.path.abspath(os.path.dirname(__file__) + num_dir*'/..') # absolute path to plasmetry/src
+    # absolute path to plasmetry/src
+    src_abs = os.path.abspath(os.path.dirname(__file__) + num_dir*'/..')
     print(f"Path Hammer: {src_abs}")
     split = src_abs.split('\\')     # separate path into folders for validation
     assert split[-2] == 'plasmetry' and split[-1] == 'src'  # validate correct top folder
     
-    targets = [x[0] for x in os.walk(src_abs) if x[0].split('\\')[-1]!='__pycache__'] # get subdirs, exclude __pycache__
+    # get subdirs, exclude __pycache__
+    targets = [x[0] for x in os.walk(src_abs) if x[0].split('\\')[-1]!='__pycache__'] 
     for dir in targets: sys.path.append(dir)    # add all subdirectories to python path
     print(f"Path Hammer: subdirectories appended to python path")
 # ----- END PATH HAMMER ----- #
