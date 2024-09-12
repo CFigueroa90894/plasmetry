e13f118aff338f2ed8f0f2fe4de0b7d10222a26d
Alberto <albertocases@e-3consulting.com>
2024-09-12 14:56:04 -0400

    file_upload updates

--- src/file_upload/data_formating.py
+++ src/file_upload/data_formating.py
@@ -1,12 +1,23 @@
-def process_data(self, unformatted_data):
+
+def is_tlp(unformatted_data):
+    
+    """"""
+    
+    if 'Bias 2' in unformatted_data:
+        return True
+    else:
+        return None
+    
+def process_data(unformatted_data):
         
         """"""
         sweep_data = []
         calculated_parameters = []
-        
+        tlp = is_tlp(unformatted_data)
         for experiment_run in unformatted_data:
-            if not self.is_tlp():
-               self.sweep_data.append({'Bias' : experiment_run['Bias 1'], \
+            print (experiment_run)
+            if not tlp:
+               sweep_data.append({'Bias' : experiment_run['Bias 1'], \
                                        'Raw Signal' : experiment_run['Raw voltage 1'],\
                                        'Filtered current': experiment_run['Filtered current']})
                del experiment_run['Bias 1']
@@ -15,12 +26,3 @@ def process_data(self, unformatted_data):
             calculated_parameters.append(experiment_run)
         return sweep_data, calculated_parameters
     
-def is_tlp(self, unformatted_data):
-    
-    """"""
-    
-    if 'Bias 2' in unformatted_data:
-        return True
-    else:
-        return None
-        
--- src/file_upload/file_upload.py
+++ src/file_upload/file_upload.py
@@ -1,9 +1,26 @@
+import os
+import sys
+
+# ----- PATH HAMMER v2.4 ----- resolve absolute imports ----- #
+if __name__ == "__main__":  # execute snippet if current script was run directly 
+    num_dir = 1           # how many parent folders to reach /plasmetry/src
+
+    src_abs = os.path.abspath(os.path.dirname(__file__) + num_dir*'/..') # absolute path to plasmetry/src
+    print(f"Path Hammer: {src_abs}")
+    split = src_abs.split('\\')     # separate path into folders for validation
+    assert split[-2] == 'plasmetry' and split[-1] == 'src'  # validate correct top folder
+    
+    targets = [x[0] for x in os.walk(src_abs) if x[0].split('\\')[-1]!='__pycache__'] # get subdirs, exclude __pycache__
+    for dir in targets: sys.path.append(dir)    # add all subdirectories to python path
+   # print(f"Path Hammer: subdirectories appended to python path")
+# ----- END PATH HAMMER ----- #
+
 import csv
 from data_formating import process_data
 
 class FileUpload:
-    
-    def __init__(self, local_path, unformatted_data=None, credentials_path=None):
+      
+    def __init__(self, local_path, unformatted_data='', credentials_path=''):
         
         """FileUpload construtor"""
         
@@ -17,6 +34,7 @@ class FileUpload:
     def new_upload(self, parameters):
         
         """"""
+        
         # Storing unformatted parameters dictionary
         self.sweep_data, self.calculated_parameters = process_data(parameters)
     
@@ -24,30 +42,88 @@ class FileUpload:
         
         """"""
         
-        time_stamp = 10
-        self.local_path = self.local_path + str(time_stamp)
-        self.local_file_write(self.calculated_parameters)
-        
+        time_stamp = 1
+        self.local_path = self.local_path + str(time_stamp) + ' testing'
+        self.local_file_write(self.calculated_parameters, self.local_path + ' parameters.csv' )
         if  self.sweep_data: 
-            self.local_file_write(self.sweep_data)
+            self.local_file_write(self.sweep_data, self.local_path + ' sweepdata.csv')
             
-    def local_file_write(self, data):
+    def local_file_write(self, data, path_name):
         
         """"""
 
         keys = list(data[0].keys())
-        with open(self.local_path, "w", newline='') as csv_file:
+        with open(path_name, "w", newline='') as csv_file:
             dict_writer= csv.DictWriter(csv_file, keys)
             dict_writer.writeheader()
             dict_writer.writerows(data)
-            
+
     
         
     
-            
+""" Sample usage of the equations. Requires protected_dictionary to be loaded in memory."""    
+if __name__ == "__main__": 
     
+    import numpy as np
+    from slp_plasma_parameters import get_equations
+    
+    def LoadPreviousData():
+        
+        """Function to load data from previous implementation. Code developed by Felix Cuadrado"""
+        
+        import csv as csv_library
+        with open('Feliz_A1 MirorSLP120200813T105858.csv', newline='') as csv:
+            dataReader = csv_library.reader(csv, delimiter=',', quotechar='|')
+            next(dataReader)  # Skip the header row
+            current = []
+            voltageSLP = []
+            for row in dataReader:
+                try:
+                    current = np.append(current, float(row[0]))
+                    voltageSLP = np.append(voltageSLP, float(row[1]))
+                except:
+                    None    
+            
+            return voltageSLP, current
+    
+    
+    def set_params():
+        # Parameter dictionary, stores parameters
+        parameters= {}
+        
+        # Storing bias and raw current lists from previous implementation
+        parameters['Bias 1'], parameters['Raw voltage 1'] =  LoadPreviousData()
+        
+        # Storing Probe area of a previous implementation, and ion mass in kg of argon, 
+        # simulating config values
+        parameters['config_ref'] = {'Probe area' : 30.3858e-06, 'Particle mass': 6.629e-26, 'Shunt 1': 1}
+        return parameters
+    
+    def calc_params(parameters):
+        # Running each equation
+        list_of_equations = get_equations()
+        
+        for i in list_of_equations[:len(list_of_equations)-1]:
+            i(parameters)
+            
+        return parameters
+            
+        
+        
+        
+    parameters = set_params()
 
-
+    list_of_parameters = []
+    
+    list_of_parameters.append(calc_params(parameters))
+    
+    parameters = set_params()
+    
+    list_of_parameters.append(calc_params(parameters))
+        
+    upload_object = FileUpload('testing path/', list_of_parameters)
+    upload_object.local_upload()
+    
  
         
         
