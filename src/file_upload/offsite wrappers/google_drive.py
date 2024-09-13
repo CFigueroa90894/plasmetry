8a68fdc9c304801cfc157002bb6f920be6d87678
Alberto <albertocases@e-3consulting.com>
2024-09-13 10:42:59 -0400

    google_drive.py valid_internet_connection method def

--- src/file_upload/offsite wrappers/google_drive.py
+++ src/file_upload/offsite wrappers/google_drive.py
@@ -18,21 +18,43 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseUpload
 from google.oauth2 import service_account
 from offsite_upload import OffsiteUpload
+import socket
+
+
 
 
 class GoogleDrive(OffsiteUpload):
     
     def __init__(self, credentials_path='client_secrets.json'):
         
+        """"""
+        
         self.credentials_path = credentials_path
         self.creds = self.authenticate_connection()
-    
+        
+    def valid_internet_connection(self):
+        
+        """"""
+        
+        try:
+            socket.getaddrinfo('google.com', 443)
+            return True
+        except socket.gaierror:
+            print('Cannot connect to Google.com, verify your internet connection.')
+            return None
+            
     def authenticate_connection(self):
+        
+        """"""
+        
         SCOPES = ['https://www.googleapis.com/auth/drive']
         creds = service_account.Credentials.from_service_account_file(self.credentials_path, scopes=SCOPES)
         return creds
 
     def put_request(self, csv_obj, file_name):
+        
+        """"""
+        
         PARENT_FOLDER_ID = "17q9inqrXiG9TSLRkPErM5993wsiBJj5i"
         service = build('drive', 'v3', credentials= self.creds)
 
@@ -43,8 +65,11 @@ class GoogleDrive(OffsiteUpload):
         }
        
         media = MediaIoBaseUpload(io.BytesIO(csv_obj.encode('utf-8')), mimetype='text/csv', resumable=True)
-
+        
         file = service.files().create(
             body=file_metadata,
             media_body=media
         ).execute()
+        
+    
+
