388aec04c93f5a9ac07c9fcd3e159e882ce82898
Alberto <albertocases@e-3consulting.com>
2024-09-13 00:07:21 -0400

    Update sample_usage.py

--- src/file_upload/sample_usage.py
+++ src/file_upload/sample_usage.py
@@ -25,7 +25,7 @@ def LoadPreviousData():
     """Function to load data from previous implementation. Code developed by Felix Cuadrado"""
     
     import csv as csv_library
-    with open('Feliz_A1 MirorSLP120200813T105858.csv', newline='') as csv:
+    with open('testing path/Feliz_A1 MirorSLP120200813T105858.csv', newline='') as csv:
         dataReader = csv_library.reader(csv, delimiter=',', quotechar='|')
         next(dataReader)  # Skip the header row
         current = []
