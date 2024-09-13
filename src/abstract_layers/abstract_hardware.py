98abf0e34b266e3dcab9d1c49576b8eccfdd7454
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-09-13 17:57:08 -0400

    Changed all abstract layers to inherit from AbstractBaseLayer.

--- src/abstract_layers/abstract_hardware.py
+++ src/abstract_layers/abstract_hardware.py
@@ -14,10 +14,29 @@ status: WIP
 """
 
 # built-in imports
-from abc import ABCMeta, abstractmethod
+import sys
+import os
 
+from abc import abstractmethod
 
-class AbstractHardware(metaclass=ABCMeta):
+# ----- PATH HAMMER v2.7 ----- resolve absolute imports ----- #
+def path_hammer(num_dir:int, root_target:list[str], exclude:list[str], suffix:str="") -> None:  # execute snippet if current script was run directly 
+    """Resolve absolute imports by recusring into subdirectories and appending them to python path."""
+    src_abs = os.path.abspath(os.path.dirname(__file__) + num_dir*'/..' + suffix)
+    assert src_abs.split('\\')[-1*len(root_target):] == root_target   # validate correct top folder
+    
+    dirs = [sub[0] for sub in os.walk(src_abs) if sub[0].split('\\')[-1] not in exclude] # get subdirs, exclude unwanted
+    for dir in dirs: sys.path.append(dir)    # add all subdirectories to python path
+    print(f"Path Hammer: {src_abs}")
+
+if __name__ == "__main__":  # execute path hammer if this script is run directly
+    path_hammer(2, ['plasmetry', 'src'], ['__pycache__'], suffix='/src')  # hammer subdirs in plasmetry/src
+# ----- END PATH HAMMER ----- #
+
+# local imports
+from abstract_base_layer import AbstractBaseLayer
+
+class AbstractHardware(AbstractBaseLayer):
     """This class defines all public methods that 'Layer 4 - Hardware Interface' implementations
     must expose to upper layers.
     
