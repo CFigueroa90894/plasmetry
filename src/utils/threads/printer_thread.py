37867ce7455309560206ba38d27ae27b6a2519d8
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-09-15 16:26:03 -0400

    Moved the SayWriter class from printer_thread.py to a new file, say_writer.py.

--- src/utils/threads/printer_thread.py
+++ src/utils/threads/printer_thread.py
@@ -1,10 +1,7 @@
 """ G3 - Plasma Devs
 Utilities - Printer Thread
-    Provides printing utilities, including a printer thread and a writer object. 
-    
-    The sole responsibility of the PrinterThread is outputting buffered in a thread-safe manner.
-    The SayWriter object provides functionality to output text to various I/O objects, as well
-    as well as outputting text to the PrinterThreads buffer.
+    Provides a class, PrinterThread, that provides thread-safe text output for objects and scripts.
+    The PrinterThread reads text from a queue and outputs it so the specified object.
 
 author: figueroa_90894@students.pupr.edu
 status: WIP
@@ -20,12 +17,8 @@ classes:
 import sys
 import os
 
-from io import TextIOWrapper
-
 from threading import Event
-from queue import Queue, Empty
-
-from typing import Tuple
+from queue import Empty
 
 
 # ----- PATH HAMMER v2.7 ----- resolve absolute imports ----- #
@@ -52,7 +45,7 @@ class PrinterThread(BaseThread):
     def __init__(self, kill:Event, *args, **kwargs):
         """<...>"""
         super().__init__(*args, **kwargs)
-        self._whisper = self._print_buff
+        self._whisper = self._print_buff    # set the printing mode
         self.kill = kill                    # signal to terminate thread
 
     # ----- MAIN ----- #
@@ -83,104 +76,6 @@ class PrinterThread(BaseThread):
         """<...>"""
         super().say(msg)
 
-
-class SayWriter:
-    """A basic printing object.
-    
-    Abstracts away the need for other objects to manually track which output they should print to. 
-    It is primarily intended to be instantiated by a PrinterThread object, though in its absence
-    the parent script should instantiate the SayWriter object. The SayWriter object should be passed
-    down the stack so all of Plasmetry's subcomponents can access it.
-
-    The SayWriter object overloads the python magic method __call__, meaning that instantiated
-    objects of the class can be invoked as if they were functions. Doing so invokes the __call__
-    method, which calls printing method defined by the object's mode.
-
-    Usage Example:
-        say = SayWriter()   # instantiate the object
-        say("Hello World!") # call the object as a function
-    
-    Attributes:
-        + end - terminator for Text I/O outputs (default '\\n')
-        - __buffer: Queue | TextIOWrapper - object to output text
-    
-    Mehtods:
-        + set_buffer() - changes the printing mode of object
-        + __call__() - method called when a SayWriter object is invoked as callable object
-        + get_mode() - returns the current mode andused buffer
-        - __default() - the default printing method, uses built-in 'print()'
-        - __io_write() - write text to the Text I/O object
-        - __queue_msg() - enqueue text to the Queue buffer
-        - __whisper() - method redefined based on printing mode
-    """
-    # ----- INITILIZATION ----- #
-    def __init__(self, buffer:Queue | TextIOWrapper=None):
-        """Constructor for the SayWriter class."""
-        self.set_buffer(buffer)  # validate argument and set output mode
-        self.end = '\n'          # set line terminator, default '\n'
-
-    def set_buffer(self, buffer:Queue | TextIOWrapper=None):
-        """Sets the SayWriter object's output mode based on given arguments.
-        
-        buffer: Queue | TextIOWrapper - output buffer object
-            None (default) - sets output mode to use the built-in 'print()' method
-            Queue - sets output mode to enqueue messages in the given Queue object
-            TextIOWrapper - sets mode to write to the given I/O object
-        """
-        # Use given Queue object to enqueue text to print
-        if isinstance(buffer, Queue):
-            self.__mode = "Queue"               # used when returning mode info
-            self.__whisper = self.__queue_msg   # redefine printing method to Queue version
-        
-        # Use the given Text I/O object
-        elif isinstance(buffer, TextIOWrapper):
-            self.__mode = "Text I/O"            # used when returning mode info
-            self.__whisper = self.__io_write    # redefine printing method to Text I/O version
-
-        # Use default if no buffer was specified (or it is an unsupported type)
-        elif buffer is None:
-            self.__mode = "Default"             # returned for mode info
-            self.__whisper = self.__default     # redefine printing method to default version
-
-        # Error handling
-        else:
-            raise TypeError(f"Argument type {type(buffer)} is not supported!")
-        
-        # Save argument
-        self.__buffer = buffer
-        
-    # ----- PUBLIC UTILS ----- #
-    def __call__(self, msg) -> None:
-        """Outputs text using the method defined by the object's mode. Allows SayWriter
-        objects to be called as if they were a function. See the class docstring for usage.
-        """
-        self.__whisper(msg)
-    
-    def get_mode(self) -> Tuple[str, object]:
-        """Returns a tuple with the object's mode and its buffer."""
-        return (self.__mode, self.__buffer)
-    
-    # ----- PRIVATE UTILS ----- #
-    def __default(self, msg) -> None:
-        """Prints text directly to the python interpreter's output. By default, the python
-        interpreter wraps stdout with the built-in print() method.
-        """
-        print(msg, flush=True)      # default built-in method
-
-    def __io_write(self, msg) -> None:
-        """<...>"""
-        self.__buffer.write(f"{msg}{self.end}")    # write to the Text I/O object
-        self.__buffer.flush()       # flush the TextI/O object's buffer
-
-    def __queue_msg(self, msg) -> None:
-        """Places the text in a thread-safe queue for the PrinterThread."""
-        self.__buffer.put(msg)      # enqueue text
-
-    def __whisper(self, msg) -> None:
-        """Method invoked to print text in the __call__ method. Overriden by the object
-        with the corresponding print method depending on the object's mode
-        """
-        raise NotImplementedError("The __whisper() method was not overriden in the constructor!")
 
 # if __name__ == "__main__":
 #     import time
