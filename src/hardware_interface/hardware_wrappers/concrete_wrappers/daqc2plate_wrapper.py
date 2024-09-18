d09007c0bffc0279bdad8a9c25f9979ad20be956
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-09-18 02:31:04 -0400

    Implemented 'write_digital()' method in DAQC2plateWrapper.

--- src/hardware_interface/hardware_wrappers/concrete_wrappers/daqc2plate_wrapper.py
+++ src/hardware_interface/hardware_wrappers/concrete_wrappers/daqc2plate_wrapper.py
@@ -8,19 +8,21 @@
 import sys
 import os
 
-# external imports
-"""from <...> import <...>"""
+# third-party imports
+import piplates.DAQC2plate as dac
 
 # ----- PATH HAMMER v2.4 ----- resolve absolute imports ----- #
 if __name__ == "__main__":  # execute snippet if current script was run directly 
     num_dir = 3             # how many parent folders to reach /plasmetry/src
 
-    src_abs = os.path.abspath(os.path.dirname(__file__) + num_dir*'/..') # absolute path to plasmetry/src
+    # absolute path to plasmetry/src
+    src_abs = os.path.abspath(os.path.dirname(__file__) + num_dir*'/..')
     print(f"Path Hammer: {src_abs}")
     split = src_abs.split('\\')     # separate path into folders for validation
     assert split[-2] == 'plasmetry' and split[-1] == 'src'  # validate correct top folder
     
-    targets = [x[0] for x in os.walk(src_abs) if x[0].split('\\')[-1]!='__pycache__'] # get subdirs, exclude __pycache__
+    # get subdirs, exclude __pycache__
+    targets = [x[0] for x in os.walk(src_abs) if x[0].split('\\')[-1]!='__pycache__']
     for dir in targets: sys.path.append(dir)    # add all subdirectories to python path
     print(f"Path Hammer: subdirectories appended to python path")
 # ----- END PATH HAMMER ----- #
@@ -33,9 +35,9 @@ from type_enforcer import enforce_type
 
 class DAQC2plateWrapper(AbstractWrapper):
     """<...>"""
-    def __init__(self):
+    def __init__(self, plate_addr:int=0):
         """<...>"""
-        raise NotImplementedError
+        self.plate = plate_addr     # address of the piplate in the stack
     
     # ----- ANALOG I/0 ----- #
     @enforce_type
@@ -52,13 +54,18 @@ class DAQC2plateWrapper(AbstractWrapper):
     @enforce_type
     def write_digital(self, address:int, level:bool) -> None:
         """<...>"""
-        raise NotImplementedError
+        if level:   # set on True
+            dac.setDOUTbit(self.plate, address)
+        else:       # clear on False
+            pass
+            dac.clrDOUTbit(self.plate, address)
     
     @enforce_type
     def read_digital(self, address:int) -> bool:
         """<...>"""
         raise NotImplementedError
-    
 
+    
 # WRAPPER EXPORT
 wrapper = DAQC2plateWrapper
+
