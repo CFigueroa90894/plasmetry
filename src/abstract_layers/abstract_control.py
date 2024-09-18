c5cbcd0274de5779067f35d0883eb8c5676509bc
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-09-18 17:45:36 -0400

    Renamed 'system_shutdown()' to 'layer_shutdown()' in AbstractControl. Removed abstract method 'get_keysets()'.

--- src/abstract_layers/abstract_control.py
+++ src/abstract_layers/abstract_control.py
@@ -16,17 +16,18 @@ from threading import Event
 from typing import Tuple
 
 # ----- PATH HAMMER v2.7 ----- resolve absolute imports ----- #
-def path_hammer(num_dir:int, root_target:list[str], exclude:list[str], suffix:str="") -> None:  # execute snippet if current script was run directly 
-    """Resolve absolute imports by recusring into subdirectories and appending them to python path."""
+def path_hammer(num_dir:int, root_target:list[str], exclude:list[str], suffix:str="") -> None:
+    """Resolve absolute imports by recursing into subdirs and appending them to python path."""
     src_abs = os.path.abspath(os.path.dirname(__file__) + num_dir*'/..' + suffix)
     assert src_abs.split('\\')[-1*len(root_target):] == root_target   # validate correct top folder
     
-    dirs = [sub[0] for sub in os.walk(src_abs) if sub[0].split('\\')[-1] not in exclude] # get subdirs, exclude unwanted
+    # get subdirs, exclude unwanted
+    dirs = [sub[0] for sub in os.walk(src_abs) if sub[0].split('\\')[-1] not in exclude]
     for dir in dirs: sys.path.append(dir)    # add all subdirectories to python path
     print(f"Path Hammer: {src_abs}")
 
 if __name__ == "__main__":  # execute path hammer if this script is run directly
-    path_hammer(2, ['plasmetry', 'src'], ['__pycache__'], suffix='/src')  # hammer subdirs in plasmetry/src
+    path_hammer(2, ['plasmetry', 'src'], ['__pycache__'], suffix='/src')
 # ----- END PATH HAMMER ----- #
 
 # local imports
@@ -213,7 +214,7 @@ class AbstractControl(AbstractBaseLayer):
 
 
     @abstractmethod
-    def system_shutdown(self) -> None:
+    def layer_shutdown(self) -> None:
         """Called by upper layers to initiate a system-wide shutdown.
         
         Lower layers will attempt to complete pending operations, then terminate their processes.
@@ -233,11 +234,4 @@ class AbstractControl(AbstractBaseLayer):
     @abstractmethod
     def get_command_flags(self) -> object:
         """Returns the system's CommandFlags control object."""
-        raise NotImplementedError("This function was not overloaded in the subclass!")
-
-
-    @abstractmethod
-    def get_keysets(self) -> object:
-        """Returns the system's key sets. Used for accessing a variety of objects, including
-        but not limited to config value keys and other structured data."""
         raise NotImplementedError("This function was not overloaded in the subclass!")
