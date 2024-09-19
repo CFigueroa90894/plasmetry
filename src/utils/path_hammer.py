74caae50ef855e6382b2ce8e5d5caff0c5f5fb01
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-09-19 17:49:45 -0400

    Implemented Path Hammer v3.0 for system agnostic path resolution. Supports forward and back slahes as path delimeters.

--- src/utils/path_hammer.py
+++ src/utils/path_hammer.py
@@ -22,18 +22,30 @@
 import sys
 import os
 
-# ----- PATH HAMMER v2.7 ----- resolve absolute imports ----- #
-def path_hammer(num_dir:int, root_target:list[str], exclude:list[str], suffix:str="") -> None:  # execute snippet if current script was run directly 
-    """Resolve absolute imports by recusring into subdirectories and appending them to python path."""
+# ----- PATH HAMMER v3.0 ----- resolve absolute imports ----- #
+def path_hammer(num_dir:int, root_target:list[str], exclude:list[str], suffix:str="") -> None:
+    """Resolve absolute imports by recursing into subdirs and appending them to python path."""
+    # os delimeters
+    win_delimeter, rpi_delimeter = "\\", "/"
+
+    # locate project root
     src_abs = os.path.abspath(os.path.dirname(__file__) + num_dir*'/..' + suffix)
-    assert src_abs.split('\\')[-1*len(root_target):] == root_target   # validate correct top folder
-    
-    dirs = [sub[0] for sub in os.walk(src_abs) if sub[0].split('\\')[-1] not in exclude] # get subdirs, exclude unwanted
-    for dir in dirs: sys.path.append(dir)    # add all subdirectories to python path
     print(f"Path Hammer: {src_abs}")
 
+    # select path delimeter
+    if win_delimeter in src_abs: delimeter = win_delimeter
+    elif rpi_delimeter in src_abs: delimeter = rpi_delimeter
+    else: raise RuntimeError("Path Hammer could not determine path delimeter!")
+
+    # validate correct top folder
+    assert src_abs.split(delimeter)[-1*len(root_target):] == root_target
+    
+    # get subdirs, exclude unwanted
+    dirs = [sub[0] for sub in os.walk(src_abs) if sub[0].split(delimeter)[-1] not in exclude]
+    for dir in dirs: sys.path.append(dir)    # add all subdirectories to python path
+
 if __name__ == "__main__":  # execute path hammer if this script is run directly
-    path_hammer(2, ['plasmetry', 'src'], ['__pycache__'], suffix='/src')  # hammer subdirs in plasmetry/src
+    path_hammer(1, ['plasmetry', 'src'], ['__pycache__'])  # hammer subdirs in plasmetry/src
 # ----- END PATH HAMMER ----- #
 
 
