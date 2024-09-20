2a096a5f5ebf39f9a8ef57f21240803256ce57f2
Alberto <albertocases@e-3consulting.com>
2024-09-20 19:07:31 -0400

    config validating voltages and paths

--- src/config_manager/config_manager.py
+++ src/config_manager/config_manager.py
@@ -24,11 +24,19 @@ class ConfigManager:
         self.file_name = path_name
         self.complete_file = {}
         self.say = text_out
+        self.list_of_biases = [
+            "dac_min", "dac_max", 
+            "sweep_min", "sweep_max", "sweep_amp_min", "sweep_amp_max", 
+            "rejector_bias", "rejector_min", "rejector_max",
+            "collector_bias", "collector_bias_min", "collector_bias_max",
+            "up_amp_bias", "up_amp_min", "up_amp_max",
+            "down_amp_bias", "down_amp_max", "down_amp_min"
+            ]
     def load_config_file(self):
         
         self.say( os.path.dirname(os.path.realpath(__file__)))
 
-        if self.validate_path():
+        if self.validate_json_path(self.file_name):
             
             self.say("loading config...")
             
@@ -42,7 +50,7 @@ class ConfigManager:
  
     def save_config_file(self):
         
-        if self.validate_path():
+        if self.validate_json_path(self.file_name):
             
             self.complete_file['sys_ref'] = self.sys_ref
             self.complete_file['config_ref'] = self.config_ref
@@ -53,36 +61,39 @@ class ConfigManager:
                 json.dump(self.complete_file, config_file, indent=4)
             self.say('Successfully saved!\n')
             
-    def validate_path(self):
+    def validate_json_path(self, file_name):
         
         """Returns boolean value validating the received path."""
 
         # Check if the directory exists 
-        if  os.path.exists(self.file_name):
-            
-            if self.file_name[-5:] == '.json':
+        if  os.path.exists(file_name):
+    
+            if  file_name[-5:] == '.json':
                 return True
             else:
-                self.say('The config file path is invalid, as the config file is not a JSON file!')
+                self.say('The file path is invalid, as it is not a JSON file!')
         else:
-             self.say(f'The config file path {self.file_name} is invalid! Check that the name is set correctly.')
+             self.say(f'The file path {file_name} is invalid! Check that the name is set correctly.')
         
     def set_config(self, probe_id, key, value):
         
         if self.config_references_loaded():
            
             if key in self.config_ref.keys() and probe_id == '':
-                self.config_ref[key] = value
+                self.validate_entry(ref=self.config_ref,probe_id='', key=key, value=value)
+            
                 
             elif key in self.config_ref[probe_id].keys():
-                self.config_ref[probe_id][key] = value
+                self.validate_entry(ref=self.config_ref,probe_id=probe_id, key=key, value=value)
+            
+                
 
             elif key in self.sys_ref[probe_id].keys():
-                self.sys_ref[probe_id][key]= value 
+                self.validate_entry(ref=self.sys_ref,probe_id=probe_id, key=key, value=value)
                 
            
             else:
-                self.say('Wrong key {key} passed as argument!')
+                self.say(f'Wrong key {key} passed as argument!')
         
     def get_config(self, probe_id, key):
         
@@ -99,7 +110,44 @@ class ConfigManager:
              else:
                  self.say('wrong key!')
       
-             
+    def validate_entry(self, ref, probe_id, key, value):
+        
+        if not probe_id:
+            self.validate_paths(ref, key, value)
+            
+        elif key in self.list_of_biases:
+            self.validate_voltage(ref, probe_id, key, value)
+    
+    
+    def validate_voltage(self, ref, probe_id, key, value):
+        if key[-3:] == 'min':
+            max_key = key[:-3] + "max"
+            if ref[probe_id][max_key] >= value:
+                ref[probe_id][key]= value
+        
+        elif key[-3:] == 'max':
+             min_key = key[:-3] + "min"
+             if ref[probe_id][min_key] <= value:
+                 ref[probe_id][key] = value
+        else:
+             max_key = key[:-4] + "max"
+             min_key = key[:-4] + "min"
+             if ref[probe_id][min_key] <= value <= ref[probe_id][max_key]:
+                 ref[key]= value            
+            
+    def validate_paths(self, ref, key, value):
+        
+        if key=='credentials_path':
+            if self.validate_json_path(value):
+               self.set_value(ref, key, value)
+        else:
+            if os.path.isdir(value):
+                self.set_value(ref, key, value)
+            
+    
+    def set_value(self, ref, key, value):
+        ref[key] = value
+        
     def config_references_loaded(self):
         
         try:
