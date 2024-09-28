adae337cf14d290d88d1f787d25e9c3a053cc2c9
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-09-28 19:00:29 -0400

    Added all docstrings to system_flags.py.

--- src/control/control_utils/system_flags.py
+++ src/control/control_utils/system_flags.py
@@ -1,15 +1,43 @@
-# author: figueroa_90894@students.pupr.edu
-# status: WIP
-#   - add docstrings
+""" G3 - Plasma Devs
+Layer 2 - Control - System Flags
+    Provides two classes to quickly instantiate sets of flags used system-wide to coordinates all
+    layers of the Plasmetry Software Architecture.
+
+author: figueroa_90894@students.pupr.edu
+status: DONE
+
+Classes:
+    StatusFlags
+    CommandFlags
+
+"""
 
 # built-in
 from threading import Event
 
 
 class StatusFlags:
-    """<...>"""
+    """This class consists of seven Event flags from Python's built-in threading module. This set
+    of flags is used to throughout the layers to indicate different states they may be in.
+    
+    Attributes:
+        + operating: Indicates the physical probe circuit is enabled.
+        + calculating: Indicates plasma parameters are being calculated.
+        + connecting: Indicates a connection to the remote storage is being established.
+        + transmitting: Indicates experiment results are being uploaded to the remote storage.
+        + formatting: Indicates experiment results are being processed.
+        + loading_config_file: Indicates the config file is being read and committed to memory.
+        + writing_config_file: Indicates config values memory are being written to the config file.
+
+    Method:
+        + __init__(): Constructor for this class; generates a new set of flags.
+
+    """
     def __init__(self):
-        """<...>"""
+        """The constructor for StatusFlags, it takes no arguments. Every StatusFlags object that is
+        instantiated creates a complete new set of flags.
+        
+        """
         # Probe Operations
         self.operating = Event()    # High voltage amps are operating
         self.calculating = Event()  # Calculating plasma parameters
@@ -25,9 +53,23 @@ class StatusFlags:
 
 
 class CommandFlags:
-    """<...>"""
+    """This class consists of three Event flags from Python's built-in threading module. This set
+    of flags is used to throughout the layers to trigger different behaviors in its subsystems.
+    
+    Attributes:
+        + shutdown: Begins process of terminating the entire system.
+        + diagnose: System continues to perform data acquisition while set.
+        + refresh: Set to indicate new values for display are available. Cleared when they are read.
+
+    Method:
+        + __init__(): Constructor for this class; generates a new set of flags.
+
+    """
     def __init__(self):
-        """<...>"""
+        """The constructor for CommandFlags, it takes no arguments. Every CommandFlags object that
+        is instantiated creates a complete new set of flags.
+        
+        """
         self.shutdown = Event()     # complete pending operations then terminate
         self.diagnose = Event()     # perform diagnostics
         self.refresh = Event()      # new plasma parameters to display
