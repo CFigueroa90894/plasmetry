d1c55f6441763a4dda3bb5b017104f4dea16a080
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-09-28 20:14:25 -0400

    Added module header and class UML notation to offsite_upload.py.

--- src/control/file_upload/offsite wrappers/offsite_upload.py
+++ src/control/file_upload/offsite wrappers/offsite_upload.py
@@ -1,6 +1,29 @@
+""" G3 - Plasma Devs
+Layer 2 - Control - Offsite Upload
+    This module specifies the interface for wrappers used to upload a remote storage service.
+
+author: <----------------------
+status: <?>
+
+Classes:
+    OffsiteUpload
+
+"""
+
+
 from abc import ABC, abstractmethod
 
 class OffsiteUpload(ABC):
+    """This abstract class specifies five methods that concrete wrapper for remote storage upload
+    in order to be comptatible with the FilUpload module.
+    
+    Methods:
+        + authenticate_connection()
+        + put_request()
+        + validate_connection()
+        + create_folder()
+        + folder_exists()
+    """
     
     @abstractmethod
     def authenticate_connection(self):
