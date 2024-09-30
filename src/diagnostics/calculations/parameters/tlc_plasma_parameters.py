1db76c46c0dc6bf9ec4f1d703fba22285743449f
Alberto <albertocases@e-3consulting.com>
2024-09-30 19:07:52 -0400

    Update tlc_plasma_parameters.py

--- src/diagnostics/calculations/parameters/tlc_plasma_parameters.py
+++ src/diagnostics/calculations/parameters/tlc_plasma_parameters.py
@@ -129,7 +129,7 @@ def get_electron_density(parameters):
         return
     
     # Storing ion saturation current
-    parameters['Ion saturation current (Amperes)'] = parameters['Probe 3 filtered current (Amperes)']
+    parameters['Ion saturation current (Amperes)'] = parameters['Probe 2 filtered current (Amperes)']
     
     # Calling global electron density function, which is deployed by SLP, HEA, and IEA
     get_particle_density(parameters)
