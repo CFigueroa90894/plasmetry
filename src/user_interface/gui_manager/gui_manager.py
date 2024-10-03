9cb52e3e07790d16b4bf38b7087ec01dbf532d48
Alberto <albertocases@e-3consulting.com>
2024-10-03 11:03:15 -0400

    clean up on guimanager

--- src/user_interface/gui_manager/gui_manager.py
+++ src/user_interface/gui_manager/gui_manager.py
@@ -85,12 +85,16 @@ class GuiManager():
         # Setting up logic for signal emission
         setup_window.switch_to_run.connect(lambda: (run_window.show(), setup_window.close(),
                                                     self.control._say_obj(f"GUI: Switch to Run Page.")))
-        setup_window.switch_to_settings.connect(lambda: (settings_window.show(), setup_window.close(), settings_window.set_widget_values(), 
+        setup_window.switch_to_settings.connect(lambda: (settings_window.show(), 
+                                                         setup_window.close(), 
+                                                         settings_window.set_widget_values(), 
                                                          self.control._say_obj(f"GUI: Switch to Settings Page.")))
-        run_window.back_btn_clicked.connect(lambda: (setup_window.show(), run_window.close(), 
+        run_window.back_btn_clicked.connect(lambda: (setup_window.show(), 
+                                                     run_window.close(), 
                                                      self.control._say_obj(f"GUI: Switch to Setup Page.")))
         
-        settings_window.back_btn_clicked.connect(lambda: (setup_window.show(), settings_window.close(),
+        settings_window.back_btn_clicked.connect(lambda: (setup_window.show(), 
+                                                          settings_window.close(),
                                                           self.control._say_obj(f"GUI: Switch to Setup Page.")))
 
         
