dd7733305066df1f55a5deb732161416dbb4b1bd
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-10-20 16:21:33 -0400

    Added install and uninstall bash scripts specific to RPIplate.

---
+++ installer/plasmetry.uninstall.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/bash
+
+echo 'Uninstalling Plasmetry...'
+sudo rm -R ~/plasmetry
+sudo rm ~/Desktop/Plasmetry
+sudo rm ~/.local/share/applications/plasmetry.desktop
+echo 'Plasmetry removed.'
