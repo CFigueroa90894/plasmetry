f8e812e3dc7db48849395d9df2c6c47947c8bcff
Alberto <albertocases@e-3consulting.com>
2024-10-22 20:40:17 -0400

    Update global_parameters.py

--- src/diagnostics/calculations/parameters/global_parameters.py
+++ src/diagnostics/calculations/parameters/global_parameters.py
@@ -165,40 +165,73 @@ def get_plasma_potential(parameters):
     # Storing dI/dV
     derivative = np.gradient(filtered_current_list, voltage_list)
     
-    # Value closest to 0 is taken to yield the index
-    parameters['Plasma potential index'] =  np.argmin((derivative))
+    second_derivative = np.gradient(derivative, voltage_list)
     
-    # Storing the plasma potential
+    maximum_current= np.argmax(abs(filtered_current_list))
+    
+    
+    # Storing the index of the plasma potential and its value
+    if parameters['Filtered current (Amperes)'][maximum_current] >0:
+        # Storing the index of the plasma potential and its value
+        parameters['Plasma potential index'] = np.argmin(abs(second_derivative))
+    else:
+        parameters['Plasma potential index'] = np.argmax((second_derivative))
     parameters['Plasma potential (Volts)'] = voltage_list[parameters['Plasma potential index']]
-
+    
+    # Storing the index of the floating potential and its value
+    parameters['Minimum value index'] = np.argmin(abs(filtered_current_list)) 
+    parameters['Minimum value (Volts)'] = voltage_list[parameters['Minimum value index']]
     
 def get_particle_temperature(parameters):
+
+    """Single Langmuir Probe electron temperature in both electron volts 
     
-    """HEA and IEA particle temperature for electron volts and  Joules is yielded by this function.
-    
-    Particle temperature in electron volts may be yielded from the value of
-    
-    1/ (d(ln(I))/dV) where the plasma potential occurs.
+    and  Joules is calculated by the slp_electron_temperature function.
     """
     
-    # Storing the charge of the electron particle in in Coulumb
-    ELECTRON_CHARGE = 1.60217657e-19
+    """Electron temperature is to be yielded from the inverse value of the slope of the ln(I)-V values 
     
-    # Storing parameters used to calculate temperature
-    plasma_potential_index = parameters['Plasma potential index']
-    filtered_current_list = parameters['Filtered current (Amperes)'] 
-    voltage_list =  parameters['Bias 1']
+    between the floating and plasma potential. Since the plasma potential and floating potential are yielded 
     
-    # Storing the derivative of the ln(I)-V values
-    log_I_V_derivative=  np.gradient(np.log(abs(filtered_current_list)), voltage_list )
+    as approximations, the starting and final point used to calculate the slope of the ln(I)-V graph were chosen
     
-    # Storing the particle temperature in electron volts
-    parameters['Particle temperature (eV)'] = 1 / log_I_V_derivative[plasma_potential_index]
+    between the plasma and floating potential since these points shall be in the line that is formed in the ln(I)-V graph.
     
-    # Storing the particle temperature in Joules
-    parameters['Particle temperature (Joules)'] = parameters['Particle temperature (eV)'] * ELECTRON_CHARGE
+    This decision was made to ensure the calculations are based on the expected slope.
+    """
+    try:
+        # Storing the charge of the electron particle, since it shall be used for calculation
+        ELECTRON_CHARGE = 1.60217657e-19
 
-
+        # Storing the number of points between the plasma potential and floating potential.
+        points_number = parameters['Plasma potential index'] -  parameters['Minimum value index']
+        
+        # The starting point used calculate the slope is the point with an index 1/4 of the way
+        # between the floating potential and plasma potential.
+        starting_index = int(np.ceil(points_number * 0.25)) + parameters['Minimum value index']
+        
+        # The final point used to calculate the slope is the point with an index 3/4 of the distance 
+        # between the floating potential and plasma potential. 
+        final_index= int(np.ceil(points_number * 0.75 )) + parameters['Minimum value index']
+       
+        # To calculate the slope, the numerator and denominator shall be acquired.
+        numerator_of_slope = np.log(abs(parameters['Filtered current (Amperes)'][final_index])) - \
+        np.log(abs(parameters['Filtered current (Amperes)'] [starting_index]))
+        denominator_of_slope = abs(parameters['Bias 1'][final_index]) - parameters['Bias 1'][starting_index]
+        # Denominator / numerator is being performed 
+        parameters['Particle temperature (eV)'] =abs( denominator_of_slope / numerator_of_slope)
+        # Multiplying the electron particle mass times the electron temperature in electron volts
+        # yields the electron temperature in Joules
+        parameters['Particle temperature (Joules)'] = parameters['Particle temperature (eV)'] * ELECTRON_CHARGE
+        
+    except Exception as e: 
+        
+        # storing np.nan since there is an error raise
+        parameters['Particle temperature (eV)'] = np.nan
+        
+        # storing np.nan since there is an error raise
+        parameters['Particle temperature (Joules)'] = np.nan
+        print (f'{e}')
 def get_equations():
     
     """This function returns a list of references for the equations used by 
@@ -238,7 +271,7 @@ if __name__ == "__main__":
             voltageSLP = []
             for row in dataReader:
                 try:
-                    current = np.append(current, float(row[1])*-1)
+                    current = np.append(current, float(row[1]))
                     voltageSLP = np.append(voltageSLP, float(row[0]))
                 except:
                     None    
@@ -259,7 +292,7 @@ if __name__ == "__main__":
     # Running each equation
     list_of_equations = get_equations()
 
-    for i in list_of_equations[1:]:
+    for i in list_of_equations:
         i(parameters)
     
     
