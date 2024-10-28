1cf1ee12a4cf379de996f6984d7d7fd370aa3bba
figueroa_90894 <figueroa_90894@students.pupr.edu>
2024-10-28 15:09:38 -0400

    Updated installer to use HTTPS instead of SSH to account for repository visibility change to public.

--- installer/plasmetry.install.sh
+++ installer/plasmetry.install.sh
@@ -2,7 +2,7 @@
 
 echo 'Downloading Plasmetry - RPIplate CM4 DAQC2plate...'
 cd ~/
-git clone -b rpiplasmaplate git@github.com:CFigueroa90894/plasmetry.git
+git clone -b rpiplasmaplate https://github.com/CFigueroa90894/plasmetry.git
 
 echo 'Running setup...'
 . ~/plasmetry/.setup_scripts/plasmetry.setup.sh
